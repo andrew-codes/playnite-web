7b9203560f7b7b0c59ab7181388338ad3e206d3a
Andrew Smith <andrew@andrew.codes>
2023-11-14 14:52:27 +0000

    build target

---
+++ .yarn/unplugged/@nx-dotnet-core-virtual-aa8e78287d/node_modules/@nx-dotnet/core/src/generators/preset/generator.d.ts
@@ -0,0 +1,2 @@
+import { Tree } from '@nx/devkit';
+export default function (tree: Tree): Promise<void>;
---
+++ .yarn/unplugged/@nx-dotnet-core-virtual-aa8e78287d/node_modules/@nx-dotnet/core/src/migrations/update-1.24.0/update-1.24.0.d.ts
@@ -0,0 +1,2 @@
+import { Tree } from '@nx/devkit';
+export default function update(host: Tree): Promise<void>;
