7b5d31b44eef5051ba165d8d686f717820252fff
Andrew Smith <andrew@andrew.codes>
2023-11-26 23:37:46 +0000

    ability to run unit tests via jest

---
+++ apps/game-db-updater/jest.config.js
@@ -0,0 +1,22 @@
+const { defaults } = require("jest-config");
+
+const defaultConfig = {
+  transform: {
+    "^.+\\.(j|t)sx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  resetMocks: true,
+  modulePathIgnorePatterns: ["<rootDir>/.dist/"],
+  passWithNoTests: true,
+  coverageDirectory: "<rootDir>/.test-runs/unit",
+  collectCoverage: true,
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
+  collectCoverageFrom: ["**/src/**", "**/scripts/**"],
+  coveragePathIgnorePatterns: ["/__tests__/", "/__mocks__/"],
+};
+
+module.exports = defaultConfig;
