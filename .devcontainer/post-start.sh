9356cb957c2ea69a52c5c4e1c3724bf95ef694fd
Andrew Smith <andrew@andrew.codes>
2023-11-27 13:40:51 +0000

    ensure mongo is started with devcontainer

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -8,7 +8,6 @@ mkdir -p ../.mongodb/data
 git lfs install
 
 set -o allexport
-. ../local.env
+. $PWD/../local.env
 set +o allexport
-docker stop game-db-updater-db || true
-docker run --rm --name game-db-updater-db -d -e MONGO_INITDB_ROOT_USERNAME=$MONGO_USERNAME -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_PASSWORD -v $PWD/../../.mongodb/data:/data/db mongo:7.0.3-jammy
+docker run --name game-db-updater-db --rm -e MONGO_INITDB_ROOT_USERNAME=$MONGO_USERNAME -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_PASSWORD -v $PWD/../.mongodb/data:/data/db mongo:7.0.3-jammy
