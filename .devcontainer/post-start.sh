9c5216bfd7e39bf61b4436d6fb1590c046e83d05
Andrew Smith <andrew@andrew.codes>
2023-11-27 14:10:21 +0000

    mosquitto running for local dev

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -3,12 +3,20 @@ set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-mkdir -p ../.mongodb/data
-
 git lfs install
 
 set -o allexport
 . $PWD/../local.env
 set +o allexport
-docker stop game-db-updater-db || true
-docker run --name game-db-updater-db --rm -e MONGO_INITDB_ROOT_USERNAME=$MONGO_INITDB_ROOT_USERNAME -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD -v $PWD/../.mongodb/data:/data/db mongo:7.0.3-jammy
+
+mkdir -p ../.data/mongodb
+docker stop playnite-web-db || true
+docker container rm playnite-web-db || true
+docker run --name playnite-web-db -d -e MONGO_INITDB_ROOT_USERNAME=$MONGO_INITDB_ROOT_USERNAME -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD -v $PWD/../.data/mongodb:/data/db mongo:7.0.3-jammy
+
+mkdir -p ../.data/mosquitto/data
+mkdir -p ../.data/mosquitto/log
+docker stop playnite-web-mqtt || true
+docker container rm playnite-web-mqtt || true
+docker run --name playnite-web-mqtt -d -p 1883:1883 -v $PWD/../.data/mosquitto/log:/mosquitto/log -v $PWD/../.data/mosquitto/data:/mosquitto/data eclipse-mosquitto:2.0.18
+
