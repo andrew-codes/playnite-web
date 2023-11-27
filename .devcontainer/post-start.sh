32392d0b89f3c417edf6e9a16e4db84827cbe8a6
Andrew Smith <andrew@andrew.codes>
2023-11-27 13:26:41 +0000

    start local mongodb in dev container

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -6,3 +6,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 mkdir -p ../.mongodb/data
 
 git lfs install
+
+set -o allexport
+. ../local.env
+set +o allexport
+docker stop game-db-updater-db || true
+docker run --rm --name game-db-updater-db -d -e MONGO_INITDB_ROOT_USERNAME=$MONGO_USERNAME -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_PASSWORD -v $PWD/../../.mongodb/data:/data/db mongo:7.0.3-jammy
