c46580e4a27bd4b167c5b7a56c5666713aee78dc
Andrew Smith <andrew@andrew.codes>
2023-11-27 16:01:36 +0000

    ensure can connect to mqtt

--- apps/game-db-updater/src/__tests__/index.test.ts
+++ apps/game-db-updater/src/__tests__/index.test.ts
@@ -1,4 +1,25 @@
-test("sample test that always passes", () => {
-  expect(true).toEqual(true);
-  expect(process.env.MQTT_HOST).toEqual("localhost");
+const mockHandler = jest.fn();
+jest.mock("../handlers", () => {
+  const realHandlers = jest.requireActual("../handlers").default;
+  return realHandlers.concat([mockHandler]);
+});
+import { type AsyncMqttClient, connectAsync } from "async-mqtt";
+import sut from "..";
+import { createMqtt } from "../mqttClient";
+
+let testMqttClient: AsyncMqttClient;
+afterEach(() => {
+  testMqttClient?.end();
+});
+
+test("Connects to MQTT and subscribes to playnite/# topics.", (done) => {
+  mockHandler.mockImplementationOnce(() => {
+    expect(mockHandler).toHaveBeenCalledWith("playnite/test", "test");
+    done();
+  });
+
+  sut().then((mqttClient) => {
+    testMqttClient = mqttClient;
+    testMqttClient.publish("playnite/test", "test");
+  });
 });
