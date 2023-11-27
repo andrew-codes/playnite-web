7cb65eaaaa0c1f58a67ece45edb731071f0e07ea
Andrew Smith <andrew@andrew.codes>
2023-11-27 13:43:06 +0000

    stop mongo container prior to starting

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -10,4 +10,5 @@ git lfs install
 set -o allexport
 . $PWD/../local.env
 set +o allexport
+docker stop game-db-updater-db || true
 docker run --name game-db-updater-db --rm -e MONGO_INITDB_ROOT_USERNAME=$MONGO_USERNAME -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_PASSWORD -v $PWD/../.mongodb/data:/data/db mongo:7.0.3-jammy
