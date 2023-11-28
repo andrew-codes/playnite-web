1470ecdeddaa4c6509669a3fe006408b77f8922b
Andrew Smith <andrew@andrew.codes>
2023-11-28 15:19:46 +0000

    enable mqtt access external from dev container

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 git lfs install
 
@@ -10,18 +9,28 @@ set +o allexport
 mkdir -p .data/mongodb
 docker stop playnite-web-db || true
 docker container rm playnite-web-db || true
-docker run --name playnite-web-db -d -e MONGO_INITDB_ROOT_USERNAME=$MONGO_INITDB_ROOT_USERNAME -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD -v $PWD/.data/mongodb:/data/db mongo:7.0.3-jammy
+docker run --name playnite-web-db -d \
+  -e MONGO_INITDB_ROOT_USERNAME=$MONGO_INITDB_ROOT_USERNAME \
+  -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD \
+  -v $PWD/.data/mongodb:/data/db \
+  mongo:7.0.3-jammy
 
 mkdir -p .data/mosquitto/config
 mkdir -p .data/mosquitto/data
 mkdir -p .data/mosquitto/log
 echo -e "persistence true
-persistence_location .data/mosquitto/data
+persistence_location /mosquitto/data
 listener 1883
 allow_anonymous false
-password_file .data/mosquitto/config/passwd
-log_dest file .data/mosquitto/log/mosquitto.log
+password_file /mosquitto/config/passwd
 log_dest stdout" > .data/mosquitto/config/mosquitto.conf
 touch .data/mosquitto/config/passwd
 mosquitto_passwd -b .data/mosquitto/config/passwd $MQTT_USERNAME $MQTT_PASSWORD
-mosquitto --config-file .data/mosquitto/config/mosquitto.conf --daemon --verbose
+
+# mosquitto --config-file .data/mosquitto/config/mosquitto.conf --daemon --verbose
+docker build -t mqtt:latest -f .devcontainer/mqtt.Dockerfile .
+docker stop playnite-web-mqtt || true
+docker container rm playnite-web-mqtt || true
+docker run --rm --name playnite-web-mqtt -d \
+  -p 1883:1883 -p 9001:9001 \
+  mqtt:latest mosquitto -c /mosquitto/config/mosquitto.conf
