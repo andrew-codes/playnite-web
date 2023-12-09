ee5157446c0e26559ba4478eee3f1b1a78210f8e
Andrew Smith <andrew@andrew.codes>
2023-12-09 20:29:39 +0000

    mongod, fix mqtt local

--- apps/game-db-updater/src/mqttClient.ts
+++ apps/game-db-updater/src/mqttClient.ts
@@ -1,17 +1,30 @@
-import { type AsyncMqttClient, connectAsync } from "async-mqtt";
+import { connectAsync, type AsyncMqttClient } from "async-mqtt";
 import createDebugger from "debug";
 
 let mqttClient: AsyncMqttClient;
 
-const createMqtt = async (
-  host: string = "localhost",
-  port: number = 1883,
-  username: string = "",
-  password: string = ""
+type MqttConnectionOptions = {
+  host?: string;
+  port?: number;
+  username?: string;
+  password?: string;
+};
+
+const getMqttClient = async (
+  connectionOptions?: MqttConnectionOptions
 ): Promise<AsyncMqttClient> => {
   const debug = createDebugger("game-db-updater/mqttClient");
 
   if (!mqttClient) {
+    const host =
+      connectionOptions?.host ?? process.env.MQTT_HOST ?? "localhost";
+    const port =
+      connectionOptions?.port ?? parseInt(process.env.MQTT_PORT ?? "1883", 10);
+    const username =
+      connectionOptions?.username ?? process.env.MQTT_USERNAME ?? "";
+    const password =
+      connectionOptions?.password ?? process.env.MQTT_PASSWORD ?? "";
+
     debug(
       `Existing MQTT client not found; creating one with the following options: host=${host}, port=${port}, username=${username}`
     );
@@ -26,4 +39,4 @@ const createMqtt = async (
   return mqttClient;
 };
 
-export { createMqtt };
+export { getMqttClient };
