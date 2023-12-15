d9979675819aafa50abfbd15a6395b3d9fe7bbbb
Andrew Smith <andrew@andrew.codes>
2023-12-15 16:58:49 +0000

    hello world remix app

---
+++ apps/playnite-web/server.mjs
@@ -0,0 +1,16 @@
+import { createRequestHandler } from '@remix-run/express'
+import { broadcastDevReady } from '@remix-run/node'
+import express from 'express'
+import * as build from './build/index.js'
+
+const app = express()
+app.use(express.static('public'))
+
+app.all('*', createRequestHandler({ build }))
+
+app.listen(3000, () => {
+  if (process.env.NODE_ENV === 'development') {
+    broadcastDevReady(build)
+  }
+  console.log('App listening on http://localhost:3000')
+})
