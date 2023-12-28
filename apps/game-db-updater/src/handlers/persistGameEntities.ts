98b0e3e83e28a497a5cf394e0235a8284cf09706
Andrew Smith <andrew@andrew.codes>
2023-12-28 20:15:03 +0000

    fix: use mongo correctly

--- apps/game-db-updater/src/handlers/persistGameEntities.ts
+++ apps/game-db-updater/src/handlers/persistGameEntities.ts
@@ -1,6 +1,4 @@
 import createDebugger from 'debug'
-import { merge } from 'lodash'
-import { ObjectId } from 'mongodb'
 import type { IHandlePublishedTopics } from '../IHandlePublishedTopics'
 import { getDbClient } from '../dbClient'
 
@@ -9,35 +7,30 @@ const debug = createDebugger('game-db-updater/handler/persistGameEntities')
 const topicMatch = /^playnite\/.*\/entity\/(?<entityType>.*)\/(?<entityId>.*)$/
 
 const handler: IHandlePublishedTopics = async (topic, payload) => {
-    if (!topicMatch.test(topic)) {
-        return
-    }
+  if (!topicMatch.test(topic)) {
+    return
+  }
 
-    const match = topicMatch.exec(topic)
-    if (!match?.groups) {
-        return
-    }
+  const match = topicMatch.exec(topic)
+  if (!match?.groups) {
+    return
+  }
 
-    const { entityType, entityId } = match.groups
-    const entity = JSON.parse(payload.toString())
-    debug(
-        `Persisting game entity ${entityType} with id ${entityId} received: ${JSON.stringify(
-            entity,
-            null,
-            2,
-        )}`,
-    )
-    const _id = new ObjectId(entityId)
+  const { entityType, entityId } = match.groups
+  const entity = JSON.parse(payload.toString())
+  debug(
+    `Persisting game entity ${entityType} with id ${entityId} received: ${JSON.stringify(
+      entity,
+      null,
+      2,
+    )}`,
+  )
 
-    const client = await getDbClient()
-    client
-        .db('games')
-        .collection(entityType)
-        .updateOne(
-            { _id: _id },
-            { $set: merge({}, entity, { _id }) },
-            { upsert: true },
-        )
+  const client = await getDbClient()
+  client
+    .db('games')
+    .collection(entityType)
+    .updateOne({ id: entityId }, { $set: entity }, { upsert: true })
 }
 
 export default handler
