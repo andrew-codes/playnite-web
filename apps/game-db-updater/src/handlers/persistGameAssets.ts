53aa47db431b5ae308db4067cc4fd5f4cb4837f1
Andrew Smith <andrew@andrew.codes>
2023-12-28 20:15:03 +0000

    fix: use mongo correctly

--- apps/game-db-updater/src/handlers/persistGameAssets.ts
+++ apps/game-db-updater/src/handlers/persistGameAssets.ts
@@ -6,37 +6,36 @@ import { getDbClient } from '../dbClient'
 const debug = createDebugger('game-db-updater/handler/persistGameEntities')
 
 const topicMatch =
-    /^playnite\/.*\/entity\/(?<entityType>.*)\/(?<entityId>.*)\/asset\/(?<assetId>.*)$/
+  /^playnite\/.*\/entity\/(?<entityType>.*)\/(?<entityId>.*)\/asset\/(?<assetId>.*)$/
 
 const handler: IHandlePublishedTopics = async (topic, payload) => {
-    if (!topicMatch.test(topic)) {
-        return
-    }
+  if (!topicMatch.test(topic)) {
+    return
+  }
 
-    const match = topicMatch.exec(topic)
-    if (!match?.groups) {
-        return
-    }
+  const match = topicMatch.exec(topic)
+  if (!match?.groups) {
+    return
+  }
 
-    const { assetId, entityType, entityId } = match.groups
-    debug(
-        `Persisting game entity asset ${entityType} with id ${entityId} and with asset ID ${assetId}`,
-    )
-    const binaryFile = new Binary(payload)
-    const _id = new ObjectId(assetId)
-    const relatedId = new ObjectId(entityId)
-    const assetDoc = {
-        _id: new ObjectId(assetId),
-        relatedId,
-        relatedType: entityType,
-        file: binaryFile,
-    }
+  const { assetId, entityType, entityId } = match.groups
+  debug(
+    `Persisting game entity asset ${entityType} with id ${entityId} and with asset ID ${assetId}`,
+  )
+  const binaryFile = new Binary(payload)
+  const _id = new ObjectId(assetId)
+  const relatedId = new ObjectId(entityId)
+  const assetDoc = {
+    relatedId,
+    relatedType: entityType,
+    file: binaryFile,
+  }
 
-    const client = await getDbClient()
-    client
-        .db('games')
-        .collection('assets')
-        .updateOne({ _id: _id }, { $set: assetDoc }, { upsert: true })
+  const client = await getDbClient()
+  client
+    .db('games')
+    .collection('assets')
+    .updateOne({ id: entityId }, { $set: assetDoc }, { upsert: true })
 }
 
 export default handler
