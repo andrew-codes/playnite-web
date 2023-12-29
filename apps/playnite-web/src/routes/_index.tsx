1a0106a51bad55b3f8adc900cba76d70813b99e2
Andrew Smith <andrew@andrew.codes>
2023-12-29 17:49:47 +0000

    game list layout

--- apps/playnite-web/src/routes/_index.tsx
+++ apps/playnite-web/src/routes/_index.tsx
@@ -1,18 +1,76 @@
+import type { LoaderFunctionArgs } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import useDimensions from 'react-use-dimensions'
 import { styled } from 'styled-components'
+import GameList from '../components/GameList.js'
 
-const Main = styled.main`
-  background-color: hotpink;
-`
-const Title = styled.h1`
-  font-family: monospace;
-`
+async function loader({ request }: LoaderFunctionArgs) {
+  const games: any[] = [
+    {
+      id: '1',
+      name: 'title 1',
+    },
+    {
+      id: '2',
+      name: 'title 1',
+    },
+    {
+      id: '3',
+      name: 'title 1',
+    },
+    {
+      id: '4',
+      name: 'title 1',
+    },
+    {
+      id: '5',
+      name: 'title 1',
+    },
+    {
+      id: '6',
+      name: 'title 1',
+    },
+    {
+      id: '7',
+      name: 'title 1',
+    },
+    {
+      id: '8',
+      name: 'title 1',
+    },
+    {
+      id: '9',
+      name: 'title 1',
+    },
+    {
+      id: '10',
+      name: 'title 1',
+    },
+    {
+      id: '11',
+      name: 'title 1',
+    },
+  ]
+
+  return json({
+    games,
+  })
+}
+
+const Main = styled.main``
 
 function Index() {
+  const { games } = useLoaderData<typeof loader>()
+
+  const [ref, { width }] = useDimensions()
+
   return (
-    <Main>
-      <Title>Welcome Playnite Web!</Title>
+    <Main ref={ref}>
+      <GameList width={width} columns={4} games={games} />
     </Main>
   )
 }
 
 export default Index
+export { loader }
