3e2f15788e1c6be85d2b9e6f6f939ea764b7b33a
Andrew Smith <andrew@andrew.codes>
2024-01-02 22:01:08 +0000

    enable horizontal scrolling when browsing

--- apps/playnite-web/src/routes/browse.tsx
+++ apps/playnite-web/src/routes/browse.tsx
@@ -1,6 +1,7 @@
 import type { LoaderFunctionArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
+import { useMemo } from 'react'
 import useDimensions from 'react-use-dimensions'
 import { styled } from 'styled-components'
 import PlayniteApi from '../api'
@@ -23,23 +24,60 @@ async function loader({ request }: LoaderFunctionArgs) {
     return 0
   })
 
+  const url = new URL(request.url)
+  const width = url.searchParams.get('width')
+  const height = url.searchParams.get('height')
+
   return json({
     games,
+    width: !!width ? parseInt(width) : null,
+    height: !!height ? parseInt(height) : null,
   })
 }
 
 const Main = styled.main``
 
+const spacing = 8
+const maxGameWidth = 300
+const maxGameHeight = (maxGameWidth * 4) / 3
+
 function Index() {
-  const { games } = useLoaderData<typeof loader>() as unknown as {
+  const { games, height, width } = useLoaderData<
+    typeof loader
+  >() as unknown as {
     games: Game[]
+    width: number
+    height: number
   }
 
-  const [ref, { width }] = useDimensions()
+  const [ref, { width: actualWidth, height: actualHeight }] = useDimensions()
+
+  const [rows, columns] = useMemo(() => {
+    if (width && height) {
+      const rows = Math.floor(height / maxGameHeight)
+      const columns = Math.floor(width / maxGameWidth)
+      return [rows, columns]
+    }
+
+    if (actualWidth && actualHeight) {
+      const rows = Math.floor(actualHeight / maxGameHeight)
+      const columns = Math.floor(actualWidth / maxGameWidth)
+      return [rows, columns]
+    }
+
+    return [6, 12]
+  }, [width, height, actualWidth, actualHeight])
 
   return (
     <Main ref={ref}>
-      <GameList width={width} columns={12} games={games} />
+      <GameList
+        rows={rows}
+        columns={columns}
+        games={games}
+        maxGameHeight={maxGameHeight - spacing * 2}
+        maxGameWidth={maxGameWidth - spacing * 2}
+        spacing={spacing}
+      />
     </Main>
   )
 }
