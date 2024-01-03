a48a858a67dd48baa153d6b456e816baae228ff6
Andrew Smith <andrew@andrew.codes>
2024-01-03 21:51:30 +0000

    Navigation to views Fixes #74

--- apps/playnite-web/src/routes/coverArt.$oid.tsx
+++ apps/playnite-web/src/routes/coverArt.$oid.tsx
@@ -1,8 +1,8 @@
 import { LoaderFunctionArgs } from '@remix-run/node'
 import createDebugger from 'debug'
 import { $params } from 'remix-routes'
-import PlayniteApi from '../api/playnite'
-import Oid from '../api/playnite/Oid'
+import PlayniteApi from '../api/server/playnite'
+import Oid from '../api/server/playnite/Oid'
 
 const debug = createDebugger('playnite-web/route/coverArt')
 
