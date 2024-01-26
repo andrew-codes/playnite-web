2cc1c697a69efc1bb27da8805f2ab0b350cec791
Andrew Smith <andrew@andrew.codes>
2024-01-26 21:39:18 +0000

    fix: navigation is no longer missing

--- apps/playnite-web/src/components/WithNavigation.tsx
+++ apps/playnite-web/src/components/WithNavigation.tsx
@@ -1,35 +1,97 @@
-import { Menu as MenuIcon, PlaylistPlay, Search } from '@mui/icons-material'
 import {
-  BottomNavigation,
-  BottomNavigationAction,
+  AccountCircle,
+  Home,
+  Menu as MenuIcon,
+  PlaylistPlay,
+  Search as SearchIcon,
+} from '@mui/icons-material'
+import {
+  AppBar,
+  IconButton,
+  InputBase,
   Link,
   Menu,
   MenuItem,
-  Paper,
+  Toolbar,
+  alpha,
   styled,
 } from '@mui/material'
 import { useLocation } from '@remix-run/react'
-import { FC, MouseEvent, PropsWithChildren, useCallback, useState } from 'react'
+import _ from 'lodash'
+import {
+  ChangeEvent,
+  FC,
+  MouseEvent,
+  PropsWithChildren,
+  useCallback,
+  useState,
+} from 'react'
 import { useSelector } from 'react-redux'
 import { $path } from 'remix-routes'
 import { getIsAuthenticated } from '../api/client/state/authSlice'
+import { Game } from '../api/playnite/types'
 
-const Layout = styled('div')(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  height: '100vh',
+const { debounce, merge, stubTrue } = _
 
-  '> *:first-child': {
-    flex: 1,
-    flexDirection: 'column',
-    display: 'flex',
-    margin: '16px',
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('phone')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
   },
 }))
 
-const WithNavigation: FC<PropsWithChildren> = ({ children }) => {
-  const isAuthenticated = useSelector(getIsAuthenticated)
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}))
 
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('phone')]: {
+      width: '14ch',
+      '&:focus': {
+        width: '26ch',
+      },
+    },
+  },
+}))
+
+const Spacer = styled('div')(({ theme }) => ({ flex: 1 }))
+
+const WithNavigation: FC<
+  PropsWithChildren & {
+    onFilter?: (id: string, filterFn: (game: Game) => boolean) => void
+  }
+> = ({ children, onFilter = stubTrue }) => {
+  const handleOnChange = useCallback(
+    debounce((evt: ChangeEvent<HTMLInputElement>) => {
+      onFilter(evt.target.value, (game) =>
+        game.name.toLowerCase().includes(evt.target.value.toLowerCase()),
+      )
+    }, 850),
+    [],
+  )
+
+  const { pathname } = useLocation()
+  const isAuthenticated = useSelector(getIsAuthenticated)
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
   const open = Boolean(anchorEl)
   const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
@@ -39,50 +101,35 @@ const WithNavigation: FC<PropsWithChildren> = ({ children }) => {
     setAnchorEl(null)
   }
 
-  const { pathname } = useLocation()
-  const [value, setValue] = useState(pathname)
-  const handleBottomNavigationChange = useCallback<
-    (event: React.SyntheticEvent, value: any) => void
-  >((event, newValue) => {
-    setValue(newValue)
-    if (newValue === 'more') {
-      handleClick(event as MouseEvent<HTMLButtonElement>)
-    }
-  }, [])
-
-  const location = useLocation()
-
   return (
-    <Layout>
-      <main>{children}</main>
-      <Paper
-        sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
-        elevation={3}
-      >
-        <BottomNavigation
-          showLabels
-          value={value}
-          onChange={handleBottomNavigationChange}
-        >
-          <BottomNavigationAction
-            href="/"
-            label="Playlists"
-            value="/"
-            icon={<PlaylistPlay />}
-          />
-          <BottomNavigationAction
-            label="Browse"
-            icon={<Search />}
-            href="/browse"
-            value="/browse"
-          />
-          <BottomNavigationAction
-            label="More"
-            icon={<MenuIcon />}
-            value="more"
-          />
-        </BottomNavigation>
-      </Paper>
+    <>
+      <AppBar position="sticky">
+        <Toolbar>
+          <IconButton>
+            <MenuIcon />
+          </IconButton>
+          <IconButton href="/">
+            <Home />
+          </IconButton>
+          <IconButton href="/browse">
+            <PlaylistPlay />
+          </IconButton>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              inputProps={{ 'aria-label': 'search' }}
+              placeholder="Search by name"
+              onChange={handleOnChange}
+            />
+          </Search>
+          <Spacer />
+          <IconButton onClick={handleClick}>
+            <AccountCircle />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
       <Menu
         anchorEl={anchorEl}
         open={open}
@@ -93,7 +140,7 @@ const WithNavigation: FC<PropsWithChildren> = ({ children }) => {
       >
         {!isAuthenticated && (
           <MenuItem
-            href={$path('/login', { returnTo: location.pathname })}
+            href={$path('/login', { returnTo: pathname })}
             component={Link}
           >
             Sign In
@@ -101,14 +148,15 @@ const WithNavigation: FC<PropsWithChildren> = ({ children }) => {
         )}
         {isAuthenticated && (
           <MenuItem
-            href={$path('/logout', { returnTo: location.pathname })}
+            href={$path('/logout', { returnTo: pathname })}
             component={Link}
           >
             Sign Out
           </MenuItem>
         )}
       </Menu>
-    </Layout>
+      <main>{children}</main>
+    </>
   )
 }
 
--- apps/playnite-web/src/routes/_index.tsx
+++ apps/playnite-web/src/routes/_index.tsx
@@ -2,11 +2,14 @@ import { Typography } from '@mui/material'
 import type { LoaderFunctionArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
-import { Fragment } from 'react'
+import _ from 'lodash'
+import { Fragment, useCallback, useState } from 'react'
 import PlayniteApi from '../api/playnite/index.server'
 import type { Game, Playlist } from '../api/playnite/types'
 import GameGrid from '../components/GameGrid'
 import WithNavigation from '../components/WithNavigation'
+
+const { stubTrue } = _
 
 async function loader({ request }: LoaderFunctionArgs) {
   const api = new PlayniteApi()
@@ -35,12 +38,17 @@ function Index() {
     playlists: [Playlist, Game[]][]
   }
 
+  const [[filterId, filter], setFilter] = useState<
+    [string | null, (game: Game) => boolean]
+  >([null, stubTrue as unknown as (game: Game) => boolean])
+  const handleFilter = useCallback((id, filter) => setFilter([id, filter]), [])
+
   return (
-    <WithNavigation>
+    <WithNavigation onFilter={handleFilter}>
       {playlists.map(([playlist, games]) => (
         <Fragment key={playlist.id}>
           <Typography variant="h3">{playlist.name}</Typography>
-          <GameGrid games={games} />
+          <GameGrid games={games.filter((game) => filter(game))} />
         </Fragment>
       ))}
     </WithNavigation>
