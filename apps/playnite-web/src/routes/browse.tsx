b879a9df15ead807dffe494ac5f88f411946baed
Andrew Smith <andrew@andrew.codes>
2024-01-26 21:39:18 +0000

    fix: navigation is no longer missing

--- apps/playnite-web/src/routes/browse.tsx
+++ apps/playnite-web/src/routes/browse.tsx
@@ -1,39 +1,14 @@
-import {
-  AccountCircle,
-  Menu as MenuIcon,
-  Search as SearchIcon,
-} from '@mui/icons-material'
-import {
-  AppBar,
-  IconButton,
-  InputBase,
-  Link,
-  Menu,
-  MenuItem,
-  Toolbar,
-  alpha,
-  styled,
-} from '@mui/material'
 import type { LoaderFunctionArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
-import { useLoaderData, useLocation } from '@remix-run/react'
+import { useLoaderData } from '@remix-run/react'
 import _ from 'lodash'
-import {
-  ChangeEvent,
-  MouseEvent,
-  useCallback,
-  useMemo,
-  useReducer,
-  useState,
-} from 'react'
-import { useSelector } from 'react-redux'
-import { $path } from 'remix-routes'
-import { getIsAuthenticated } from '../api/client/state/authSlice'
+import { useCallback, useMemo, useState } from 'react'
 import PlayniteApi from '../api/playnite/index.server'
 import type { Game } from '../api/playnite/types'
 import GameGrid from '../components/GameGrid'
+import WithNavigation from '../components/WithNavigation'
 
-const { debounce, merge } = _
+const { debounce, merge, stubTrue } = _
 
 async function loader({ request }: LoaderFunctionArgs) {
   const api = new PlayniteApi()
@@ -56,146 +31,24 @@ async function loader({ request }: LoaderFunctionArgs) {
   })
 }
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('phone')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}))
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}))
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  width: '100%',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    [theme.breakpoints.up('phone')]: {
-      width: '14ch',
-      '&:focus': {
-        width: '26ch',
-      },
-    },
-  },
-}))
-
-const Spacer = styled('div')(({ theme }) => ({ flex: 1 }))
-
-const searchReducer = (state, action) => {
-  switch (action.type) {
-    case 'SEARCH/QUERY_UPDATE':
-      return merge({}, state, {
-        query: action.payload,
-      })
-
-    default:
-      return state
-  }
-}
-
 function Browse() {
-  const [search, searchDispatch] = useReducer(searchReducer, {
-    query: '',
-  })
-  const handleOnChange = useCallback(
-    debounce((evt: ChangeEvent<HTMLInputElement>) => {
-      searchDispatch({
-        type: 'SEARCH/QUERY_UPDATE',
-        payload: evt.target.value,
-      })
-    }, 850),
-    [],
-  )
   const { games } = useLoaderData() as unknown as {
     games: Game[]
   }
+
+  const [[filterId, filter], setFilter] = useState<
+    [string | null, (game: Game) => boolean]
+  >([null, stubTrue as unknown as (game: Game) => boolean])
+  const handleFilter = useCallback((id, filter) => setFilter([id, filter]), [])
   const filteredGames = useMemo(
-    () =>
-      games.filter((game) => game.name.toLowerCase().includes(search.query)),
-    [games, search.query],
+    () => games.filter((game) => filter(game)),
+    [games, filterId],
   )
 
-  const { pathname } = useLocation()
-  const isAuthenticated = useSelector(getIsAuthenticated)
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-  const open = Boolean(anchorEl)
-  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
-
   return (
-    <>
-      <AppBar position="sticky">
-        <Toolbar>
-          <IconButton>
-            <MenuIcon />
-          </IconButton>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              inputProps={{ 'aria-label': 'search' }}
-              placeholder="Search by name"
-              onChange={handleOnChange}
-            />
-          </Search>
-          <Spacer />
-          <IconButton onClick={handleClick}>
-            <AccountCircle />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Menu
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        {!isAuthenticated && (
-          <MenuItem
-            href={$path('/login', { returnTo: pathname })}
-            component={Link}
-          >
-            Sign In
-          </MenuItem>
-        )}
-        {isAuthenticated && (
-          <MenuItem
-            href={$path('/logout', { returnTo: pathname })}
-            component={Link}
-          >
-            Sign Out
-          </MenuItem>
-        )}
-      </Menu>
-      <main>
-        <GameGrid games={filteredGames} />
-      </main>
-    </>
+    <WithNavigation onFilter={handleFilter}>
+      <GameGrid games={filteredGames} />
+    </WithNavigation>
   )
 }
 
