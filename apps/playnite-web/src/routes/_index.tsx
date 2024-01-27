f39dc7cacddddeffa4e1b2015530d39175362eb9
Andrew Smith <andrew@andrew.codes>
2024-01-27 17:36:54 +0000

    refactor filter and game grid props

--- apps/playnite-web/src/routes/_index.tsx
+++ apps/playnite-web/src/routes/_index.tsx
@@ -3,13 +3,16 @@ import type { LoaderFunctionArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 import _ from 'lodash'
-import { Fragment, useCallback, useState } from 'react'
+import { Fragment, useCallback, useMemo, useState } from 'react'
+import { Helmet } from 'react-helmet'
 import PlayniteApi from '../api/playnite/index.server'
-import type { Game, Playlist } from '../api/playnite/types'
 import GameGrid from '../components/GameGrid'
 import WithNavigation from '../components/WithNavigation'
+import GameList from '../domain/GameList'
+import MatchName from '../domain/playnite/matchName'
+import type { GameOnPlatform, IGameList, Playlist } from '../domain/types'
 
-const { stubTrue } = _
+const { uniqBy } = _
 
 async function loader({ request }: LoaderFunctionArgs) {
   const api = new PlayniteApi()
@@ -22,10 +25,10 @@ async function loader({ request }: LoaderFunctionArgs) {
   const upNext = allPlaylists.find(
     (playlist) => playlist.name.toLowerCase() === 'up next',
   )
-
   const playlists = [playing, upNext].filter(
     (playlist) => !!playlist,
   ) as Playlist[]
+
   const playlistGames = await api.getPlaylistsGames(playlists)
 
   return json({
@@ -35,23 +38,66 @@ async function loader({ request }: LoaderFunctionArgs) {
 
 function Index() {
   const { playlists } = useLoaderData() as unknown as {
-    playlists: [Playlist, Game[]][]
+    playlists: [Playlist, GameOnPlatform[]][]
   }
 
-  const [[filterId, filter], setFilter] = useState<
-    [string | null, (game: Game) => boolean]
-  >([null, stubTrue as unknown as (game: Game) => boolean])
-  const handleFilter = useCallback((id, filter) => setFilter([id, filter]), [])
+  const [nameQuery, setNameQuery] = useState<string>('')
+  const handleFilter = useCallback(
+    (evt, nameQuery) => setNameQuery(nameQuery),
+    [],
+  )
+  const playlistsOfGameLists: [Playlist, IGameList][] = useMemo(() => {
+    return playlists.map(([playlist, games]) => [
+      playlist,
+      new GameList(games, new MatchName(nameQuery)),
+    ])
+  }, [playlists, nameQuery])
+
+  const numberToPreload = 40
+  const [preloadGames, prefetchGames] = useMemo(() => {
+    const uniqueGames = uniqBy(
+      playlists.flatMap(([playlist, games]) => new GameList(games).games),
+      (game) => game.oid.asString,
+    )
+    return [
+      uniqueGames.slice(0, numberToPreload),
+      uniqueGames.slice(numberToPreload + 1),
+    ]
+  }, [])
 
   return (
-    <WithNavigation onFilter={handleFilter}>
-      {playlists.map(([playlist, games]) => (
-        <Fragment key={playlist.id}>
-          <Typography variant="h3">{playlist.name}</Typography>
-          <GameGrid games={games.filter((game) => filter(game))} />
-        </Fragment>
-      ))}
-    </WithNavigation>
+    <>
+      <Helmet>
+        {preloadGames.map((game) => {
+          return (
+            <link
+              key={game.oid.asString}
+              rel="preload"
+              as="image"
+              href={game.cover}
+            />
+          )
+        })}
+        {prefetchGames.map((game) => {
+          return (
+            <link
+              key={game.oid.asString}
+              rel="prefetch"
+              as="image"
+              href={game.cover}
+            />
+          )
+        })}
+      </Helmet>
+      <WithNavigation onFilter={handleFilter}>
+        {playlistsOfGameLists.map(([playlist, gameList]) => (
+          <Fragment key={playlist.id}>
+            <Typography variant="h3">{playlist.name}</Typography>
+            <GameGrid games={gameList.games} />
+          </Fragment>
+        ))}
+      </WithNavigation>
+    </>
   )
 }
 
