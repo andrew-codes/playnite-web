e4d703617e97175c8706c82ecea0ceea779175d7
Andrew Smith <andrew@andrew.codes>
2024-02-06 17:42:21 +0000

    filter games by name in my games

---
+++ apps/playnite-web/src/components/MyLibrary.tsx
@@ -0,0 +1,54 @@
+import { Typography } from '@mui/material'
+import { FC, useMemo } from 'react'
+import { Helmet } from 'react-helmet'
+import { useSelector } from 'react-redux'
+import { getFilter } from '../api/client/state/librarySlice'
+import GameGrid from '../components/GameGrid'
+import Header from '../components/Header'
+import FilteredGameList from '../domain/FilteredGameList'
+import GameList from '../domain/GameList'
+import type { GameOnPlatform } from '../domain/types'
+
+const MyLibrary: FC<{ gamesOnPlatforms: GameOnPlatform[] }> = ({
+  gamesOnPlatforms = [] as GameOnPlatform[],
+}) => {
+  const gameList = useMemo(() => {
+    return new GameList(gamesOnPlatforms)
+  }, [gamesOnPlatforms])
+
+  const filter = useSelector(getFilter)
+  const filteredGames = useMemo(
+    () => new FilteredGameList(gameList, filter),
+    [gameList, filter],
+  )
+
+  const noDeferCount = 25
+
+  return (
+    <>
+      <Helmet>
+        {gameList.items
+          .filter((game, index) => index <= noDeferCount)
+          .map((game) => (
+            <link
+              key={game.oid.asString}
+              rel="preload"
+              as="image"
+              href={game.cover}
+            />
+          ))}
+      </Helmet>
+      <Header showFilters>
+        <section>
+          <Typography variant="h2">My Games</Typography>
+          <Typography variant="subtitle1">
+            {gameList.items.length} games in my library
+          </Typography>
+        </section>
+      </Header>
+      <GameGrid games={filteredGames} noDeferCount={noDeferCount} />
+    </>
+  )
+}
+
+export default MyLibrary
--- apps/playnite-web/src/components/Search.tsx
+++ apps/playnite-web/src/components/Search.tsx
@@ -1,36 +1,24 @@
-import styled from '@emotion/styled'
-import { forwardRef, useCallback, useState } from 'react'
-
-const SearchInput = styled.input<{ height: number }>`
-  display: flex;
-  justify-content: center;
-  border-radius: ${({ height }) => height / 2}px;
-  padding: 0;
-  color: darkslategray;
-`
+import { TextField } from '@mui/material'
+import { forwardRef, useCallback } from 'react'
 
 const Search = forwardRef<
   HTMLInputElement,
   {
     onSearch: (search: string) => void
-    defaultValue: string
-    height?: number
+    defaultValue?: string
   }
->(({ onSearch, defaultValue = '', height }, ref) => {
-  const [value, setValue] = useState(defaultValue)
+>(({ onSearch, defaultValue = '' }) => {
   const handleOnChange = useCallback((e) => {
-    setValue(e.target.value)
     onSearch(e.target.value)
   }, [])
 
   return (
-    <SearchInput
-      height={height ?? 48}
+    <TextField
       onChange={handleOnChange}
-      placeholder="Search"
-      ref={ref}
+      placeholder="Find"
       type="text"
-      value={value}
+      defaultValue={defaultValue}
+      variant="outlined"
     />
   )
 })
