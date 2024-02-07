b730797bf455fb57e60266b1e644f8565e3be305
Andrew Smith <andrew@andrew.codes>
2024-02-07 21:40:39 +0000

    fix: hotfix for some introduced issues

--- apps/playnite-web/src/components/MyLibrary.tsx
+++ apps/playnite-web/src/components/MyLibrary.tsx
@@ -1,15 +1,15 @@
-import { Box, Typography, useMediaQuery, useTheme } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import _ from 'lodash'
-import { FC, useCallback, useEffect, useMemo, useRef } from 'react'
+import { FC, useMemo } from 'react'
 import { Helmet } from 'react-helmet'
-import { useDispatch, useSelector } from 'react-redux'
-import { getScrollTo, scrolledTo } from '../api/client/state/layoutSlice'
+import { useSelector } from 'react-redux'
 import { getFilter } from '../api/client/state/librarySlice'
 import GameGrid from '../components/GameGrid'
 import Header from '../components/Header'
 import FilteredGameList from '../domain/FilteredGameList'
 import GameList from '../domain/GameList'
 import type { GameOnPlatform } from '../domain/types'
+import OuterScroll from './OuterScroll'
 import useThemeWidth from './useThemeWidth'
 
 const { debounce } = _
@@ -31,31 +31,6 @@ const MyLibrary: FC<{ gamesOnPlatforms: GameOnPlatform[] }> = ({
 
   const width = useThemeWidth()
 
-  const outerScroll = useRef<HTMLDivElement>(null)
-  const scrollTo = useSelector(getScrollTo)
-  const theme = useTheme()
-  const isMobile = useMediaQuery(theme.breakpoints.down('lg'))
-  useEffect(() => {
-    if (isMobile && scrollTo === 0 && outerScroll.current) {
-      outerScroll.current.scrollTo({ top: scrollTo, behavior: 'smooth' })
-    }
-  }, [scrollTo, isMobile])
-  const dispatch = useDispatch()
-  const ticking = useRef<boolean>(false)
-  const handleScroll = useCallback(
-    debounce((evt) => {
-      if (!ticking.current) {
-        window.requestAnimationFrame(() => {
-          ticking.current = false
-          dispatch(scrolledTo(evt.target?.scrollTop))
-        })
-
-        ticking.current = true
-      }
-    }, 200),
-    [],
-  )
-
   return (
     <>
       <Helmet>
@@ -70,20 +45,7 @@ const MyLibrary: FC<{ gamesOnPlatforms: GameOnPlatform[] }> = ({
             />
           ))}
       </Helmet>
-      <Box
-        onScroll={handleScroll}
-        ref={outerScroll}
-        sx={(theme) => ({
-          display: 'flex',
-          flexDirection: 'column',
-          height: `calc(100vh - ${theme.spacing(12)})`,
-          padding: `0 ${theme.spacing()} 0 ${theme.spacing(2)}`,
-          [theme.breakpoints.down('lg')]: {
-            overflowY: 'auto',
-            scrollbarColor: `${theme.palette.text.primary} ${theme.palette.background.default}`,
-          },
-        })}
-      >
+      <OuterScroll>
         <Header showFilters>
           <div>
             <Typography variant="h2">My Games</Typography>
@@ -105,7 +67,7 @@ const MyLibrary: FC<{ gamesOnPlatforms: GameOnPlatform[] }> = ({
         >
           <GameGrid games={filteredGames} noDeferCount={noDeferCount} />
         </Box>
-      </Box>
+      </OuterScroll>
     </>
   )
 }
