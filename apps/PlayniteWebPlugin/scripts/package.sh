3ded1c35ea0ec9f0fd12aa79d13c5b6314a51ba4
Andrew Smith <andrew@andrew.codes>
2024-04-19 16:09:34 -0400

    package does not include docker build

--- apps/PlayniteWebPlugin/scripts/package.sh
+++ apps/PlayniteWebPlugin/scripts/package.sh
@@ -4,7 +4,6 @@ if [ ! "$OS" = "WindowsNT" ]; then
   exit 0
 fi
 
-rm -rf .packaged || true
 mkdir -p .packaged
 
 ../../libs/build-utils/playnite-toolbox/src/Toolbox/Toolbox.exe pack .dist/bin/Release/ ./.packaged
