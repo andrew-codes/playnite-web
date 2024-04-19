ef51b0c81766e61c53e57722b3e9faecc3cef0ec
Andrew Smith <andrew@andrew.codes>
2024-04-19 14:46:09 -0400

    improved logging for serialization errors (#250)

--- apps/PlayniteWebPlugin/src/Services/ObjectSerializer.cs
+++ apps/PlayniteWebPlugin/src/Services/ObjectSerializer.cs
@@ -1,3 +1,5 @@
+using Playnite.SDK;
+using System;
 using System.Text.Json;
 
 namespace PlayniteWeb.Services
@@ -6,7 +8,15 @@ namespace PlayniteWeb.Services
   {
     public string Serialize(object data)
     {
-      return JsonSerializer.Serialize(data, new JsonSerializerOptions(JsonSerializerDefaults.Web));
+      try
+      {
+        return JsonSerializer.Serialize(data, new JsonSerializerOptions(JsonSerializerDefaults.Web));
+      }
+      catch (Exception error)
+      {
+        LogManager.GetLogger().Error($"Serializing {data} - {error.ToString()}");
+        throw error;
+      }
     }
   }
 }
