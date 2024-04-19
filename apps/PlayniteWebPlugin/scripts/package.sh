4dfd90b43c438e89eb2fc966526ec8e0a78ba44f
Andrew Smith <andrew@andrew.codes>
2024-04-19 16:42:45 -0400

    package extension files to workflow

--- apps/PlayniteWebPlugin/scripts/package.sh
+++ apps/PlayniteWebPlugin/scripts/package.sh
@@ -1,3 +1,3 @@
 mkdir -p .packaged
 
-../../libs/build-utils/playnite-toolbox/src/Toolbox/Toolbox.exe pack .dist/bin/Release/ ./.packaged
+../../libs/build-utils/playnite-toolbox/src/Toolbox/Toolbox.exe pack .dist/bin/Release/ .packaged
