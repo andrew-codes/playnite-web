19eb1469a463b75d3aa0d59bff187b3c85504882
Andrew Smith <andrew@andrew.codes>
2024-04-19 16:34:38 -0400

    do not conditionally exit from package early

--- apps/PlayniteWebPlugin/scripts/package.sh
+++ apps/PlayniteWebPlugin/scripts/package.sh
@@ -1,9 +1,3 @@
-OS=$(uname)
-
-if [ ! "$OS" = "WindowsNT" ]; then
-  exit 0
-fi
-
 mkdir -p .packaged
 
 ../../libs/build-utils/playnite-toolbox/src/Toolbox/Toolbox.exe pack .dist/bin/Release/ ./.packaged
