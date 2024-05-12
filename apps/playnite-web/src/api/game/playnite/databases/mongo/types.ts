69e5ca5f7b6abc242e1707268ccbd6cc32a2f94b
Andrew Smith <andrew@andrew.codes>
2024-05-12 16:50:44 -0400

    fix: application Error TypeError: Cannot read properties of null (reading 'name') (#310)
    Fixes #301

--- apps/playnite-web/src/api/game/playnite/databases/mongo/types.ts
+++ apps/playnite-web/src/api/game/playnite/databases/mongo/types.ts
@@ -1,12 +1,11 @@
 import { Binary, Document, Filter, WithId } from 'mongodb'
 import type {
   GameAssetType,
-  GameOnPlatformDto,
   IIdentifyDomainObjects,
 } from '../../../../../domain/types'
 import { AssetTypeKey } from '../../../types'
 
-type PlatformData = {
+type PlatformEntity = {
   specificationId: string
   icon: string
   cover: string
@@ -20,41 +19,56 @@ type TagEntity = {
   name: string
 }
 
-type GameEntity = WithId<Document> &
-  GameOnPlatformDto & {
-    id: string
-    ageRating: string
-    backgroundImage: string
-    communityScore: number | null
-    completionStatusId: string
-    completionStatus: { id: string; name: string }
-    cover: string
-    coverImage: string
-    criticScore: number | null
-    developersIds: string[]
-    developers: { id: string; name: string }[]
-    featuresIds: string[]
-    features: { id: string; name: string }[]
-    genresIds: string[]
-    genres: { id: string; name: string }[]
-    isInstalled: boolean
-    isInstalling: boolean
-    isLaunching: boolean
-    isRunning: boolean
-    isUninstalling: boolean
-    links: { name: string; url: string }[]
-    platformsIds: string[]
-    platforms: PlatformData[]
-    publishersIds: string[]
-    publishers: { id: string; name: string }[]
-    recentActivity: string
-    releaseDate: string
-    seriesIds: string[]
-    series: { id: string; name: string }[]
-    sortingName?: string
-    sourceId: string
-    tagsIds: string[]
-  }
+type SourceEntity = {
+  id: string
+  name: string
+}
+
+type GameEntity = WithId<Document> & {
+  added: string
+  ageRating: string
+  backgroundImage: string
+  communityScore: number | null
+  completionStatus: { id: string; name: string }
+  completionStatusId: string
+  cover: string
+  coverImage: string
+  criticScore: number | null
+  description: string
+  developers: { id: string; name: string }[]
+  developersIds: string[]
+  features: { id: string; name: string }[]
+  featuresIds: string[]
+  gameId: string
+  genres: { id: string; name: string }[]
+  genresIds: string[]
+  hidden: boolean
+  id: string
+  isCustomGame: boolean
+  isInstalled: boolean
+  isInstalling: boolean
+  isLaunching: boolean
+  isRunning: boolean
+  isUninstalling: boolean
+  links: { name: string; url: string }[]
+  name: string
+  platforms: PlatformEntity[] | null
+  platformsIds: string[]
+  publishers: { id: string; name: string }[]
+  publishersIds: string[]
+  recentActivity: string
+  releaseDate: string
+  releaseYear: number
+  runState: string
+  series: { id: string; name: string }[]
+  seriesIds: string[]
+  sortingName?: string
+  sortName: string
+  source: SourceEntity | null
+  sourceId: string | null
+  tags: TagEntity[] | null
+  tagsIds: string[]
+}
 
 type GameAssetEntityType = 'games' | 'platforms'
 
@@ -85,6 +99,6 @@ export type {
   GameAssetEntityType,
   GameEntity,
   MongoDbApi,
-  PlatformData,
+  PlatformEntity,
   TagEntity,
 }
