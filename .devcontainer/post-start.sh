20642f0e59262696767e03da679eed625867afcb
Andrew Smith <andrew@andrew.codes>
2024-05-21 19:52:58 +0000

    add docker back to devpod

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -4,18 +4,17 @@ git lfs install
 
 set -o allexport
 . $PWD/local.env
-. $PWD/dev.env
 set +o allexport
 
 mkdir -p .data/mongodb
-# docker stop playnite-web-db || true
-# docker container rm playnite-web-db || true
-# docker run --rm --name playnite-web-db -d \
-#   --network host \
-#   -e MONGO_INITDB_ROOT_USERNAME=$MONGO_INITDB_ROOT_USERNAME \
-#   -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD \
-#   -v $PWD/.data/mongodb:/data/db \
-#   mongo:7.0.3-jammy
+docker stop playnite-web-db || true
+docker container rm playnite-web-db || true
+docker run --rm --name playnite-web-db -d \
+  --network host \
+  -e MONGO_INITDB_ROOT_USERNAME=$MONGO_INITDB_ROOT_USERNAME \
+  -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD \
+  -v $PWD/.data/mongodb:/data/db \
+  mongo:7.0.3-jammy
 
 mosquitto_passwd -b -c /etc/mosquitto/passwd $MQTT_USERNAME $MQTT_PASSWORD
 echo -e "allow_anonymous false
