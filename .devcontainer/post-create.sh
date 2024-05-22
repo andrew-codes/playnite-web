175dab5c59f79f63f35fa4fab94d05d73f9a73c8
Andrew Smith <andrew@andrew.codes>
2024-05-22 11:57:14 -0400

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,7 +7,24 @@ echo -e "{
 /usr/local/share/docker-init.sh
 
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
+set -o allexport
+. $PWD/local.env
+set +o allexport
+
+mosquitto_passwd -b -c /etc/mosquitto/passwd $MQTT_USERNAME $MQTT_PASSWORD
+echo -e "allow_anonymous false
+password_file /etc/mosquitto/passwd
+listener 1883" > /etc/mosquitto/conf.d/default.conf
+
+mkdir -p /.data/mongodb
+docker run --name playnite-web-db -d \
+  --network host \
+  -e MONGO_INITDB_ROOT_USERNAME=$MONGO_INITDB_ROOT_USERNAME \
+  -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD \
+  -v /.data/mongodb:/data/db \
+  mongo:7.0.3-jammy
+
+mosquitto -c /etc/mosquitto/conf.d/default.conf --daemon --verbose
 
 corepack enable
 corepack prepare --activate yarn@^4.0.0
