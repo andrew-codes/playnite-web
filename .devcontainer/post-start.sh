1364b0fc244707dedb425c8601ebc8e63c1bdcbc
Andrew Smith <andrew@andrew.codes>
2024-05-22 13:15:17 +0000

    enable mongodb in devpod

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -7,17 +7,8 @@ set -o allexport
 set +o allexport
 
 mkdir -p .data/mongodb
-docker stop playnite-web-db || true
-docker container rm playnite-web-db || true
-docker run --rm --name playnite-web-db -d \
-  --network host \
-  -e MONGO_INITDB_ROOT_USERNAME=$MONGO_INITDB_ROOT_USERNAME \
-  -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD \
-  -v $PWD/.data/mongodb:/data/db \
-  mongo:7.0.3-jammy
 
 mosquitto_passwd -b -c /etc/mosquitto/passwd $MQTT_USERNAME $MQTT_PASSWORD
 echo -e "allow_anonymous false
 password_file /etc/mosquitto/passwd
 listener 1883" > /etc/mosquitto/conf.d/default.conf
-mosquitto -c /etc/mosquitto/conf.d/default.conf --daemon --verbose
