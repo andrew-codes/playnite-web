693ed2e863a644512a05411ae87cd40fb3a64f90
Andrew Smith <andrew@andrew.codes>
2024-05-22 15:24:31 -0400

    remove mount

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -26,7 +26,6 @@ docker run --name playnite-web-db -d \
   --network host \
   -e MONGO_INITDB_ROOT_USERNAME=$MONGO_INITDB_ROOT_USERNAME \
   -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD \
-  -v /.data/mongodb:/data/db \
   mongo:7.0.3-jammy
 
 mosquitto -c /etc/mosquitto/conf.d/default.conf --daemon --verbose
