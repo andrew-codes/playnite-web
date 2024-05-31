bfb5e5a2f1f5c9a7e7e4037965b1fca99d5f91a2
Vladimir Cvetić <vladimir@ferdinand.rs>
2024-05-31 22:39:26 +0200

    Allow configuration via DB URL (#327)
    Add support for database URLs in getDbClient to enhance compatibility
    with MongoDB Atlas and similar services.
    
    Refactored the getDbClient function to accept a url parameter in the
    DbConnectionOptions. This change enables the use of MongoDB connection
    strings, which are necessary for services like MongoDB Atlas that use
    different connection schemas. By allowing the use of a full URL, we
    ensure better flexibility and compatibility with various database
    hosting services, simplifying the connection process and improving
    maintainability.

--- apps/game-db-updater/src/dbClient.ts
+++ apps/game-db-updater/src/dbClient.ts
@@ -2,6 +2,11 @@ import createDebugger from 'debug'
 import { MongoClient } from 'mongodb'
 
 let client: MongoClient
+
+type DbConnectionString = {
+  url?: string
+}
+
 type DbConnectionOptions = {
   host?: string
   port?: number
@@ -10,29 +15,34 @@ type DbConnectionOptions = {
 }
 
 const getDbClient = async (
-  connectionOptions?: DbConnectionOptions,
+  connectionOptions?: DbConnectionOptions | DbConnectionString,
 ): Promise<MongoClient> => {
   const debug = createDebugger('game-db-updater/mqttClient')
 
   if (!client) {
-    const host = connectionOptions?.host ?? process.env.DB_HOST ?? 'localhost'
-    const port =
-      connectionOptions?.port ?? parseInt(process.env.DB_PORT ?? '27017')
-    const username = connectionOptions?.username ?? process.env.DB_USERNAME
-    const password = connectionOptions?.password ?? process.env.DB_PASSWORD
+    const url = 'url' in connectionOptions ? connectionOptions.url : process.env.DB_URL
+    const host = 'host' in connectionOptions ? connectionOptions.host : process.env.DB_HOST ?? 'localhost'
+    const port = 'port' in connectionOptions ? connectionOptions.port : parseInt(process.env.DB_PORT ?? '27017')
+    const username = 'username' in connectionOptions ? connectionOptions.username : process.env.DB_USERNAME
+    const password = 'password' in connectionOptions ? connectionOptions.password : process.env.DB_PASSWORD
 
-    debug(
-      `Existing DB client not found; creating one with the following options: host=${host}, port=${port}, username=${username}`,
-    )
-    if (!username && !password) {
-      client = new MongoClient(`mongodb://${host}:${port}`)
+    if (url) {
+      debug(`Existing DB client not found; creating one with the provided URL: ${url}`)
+      client = new MongoClient(url)
     } else {
-      client = new MongoClient(`mongodb://${host}:${port}`, {
-        auth: {
-          username,
-          password,
-        },
-      })
+      debug(
+        `Existing DB client not found; creating one with the following options: host=${host}, port=${port}, username=${username}`,
+      )
+      if (!username && !password) {
+        client = new MongoClient(`mongodb://${host}:${port}`)
+      } else {
+        client = new MongoClient(`mongodb://${host}:${port}`, {
+          auth: {
+            username,
+            password,
+          },
+        })
+      }
     }
   }
   await client.connect()
