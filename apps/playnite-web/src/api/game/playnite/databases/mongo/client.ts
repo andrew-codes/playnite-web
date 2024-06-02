1f178807ea7d870db21f779ae8923ffcf181b750
Andrew Smith <andrew@andrew.codes>
2024-06-02 19:07:39 +0000

    fix TS errors manifesting in build artifacts during runtime

--- apps/playnite-web/src/api/game/playnite/databases/mongo/client.ts
+++ apps/playnite-web/src/api/game/playnite/databases/mongo/client.ts
@@ -4,45 +4,55 @@ import { MongoClient } from 'mongodb'
 let client: MongoClient
 
 type DbConnectionString = {
-  url?: string
-}
-
-type DbConnectionOptions = {
-  host?: string
-  port?: number
+  url: string
   username?: string
   password?: string
 }
 
-const getDbClient = (connectionOptions?: DbConnectionOptions | DbConnectionString): MongoClient => {
+type DbConnectionOptions = {
+  host: string
+  port: number
+  username?: string
+  password?: string
+}
+
+const computeUrl = (
+  connectionOptions?: DbConnectionOptions | DbConnectionString,
+): string => {
+  if (!connectionOptions) {
+    return `mongodb://localhost:27017`
+  }
+  if ('url' in connectionOptions) {
+    return connectionOptions.url
+  }
+
+  const host = connectionOptions.host
+  const port = connectionOptions.port
+
+  return `mongodb://$${host}:${port}`
+}
+
+const getDbClient = (
+  connectionOptions?: DbConnectionOptions | DbConnectionString,
+): MongoClient => {
   const debug = createDebugger('playnite-web-app/MongoDbClient')
 
   if (!client) {
-    const url = 'url' in connectionOptions ? connectionOptions.url : process.env.DB_URL
-    const host = 'host' in connectionOptions ? connectionOptions.host : process.env.DB_HOST ?? 'localhost'
-    const port = 'port' in connectionOptions ? connectionOptions.port : parseInt(process.env.DB_PORT ?? '27017')
-    const username = 'username' in connectionOptions ? connectionOptions.username : process.env.DB_USERNAME
-    const password = 'password' in connectionOptions ? connectionOptions.password : process.env.DB_PASSWORD
+    const url = computeUrl(connectionOptions)
+    const username = connectionOptions?.username
+    const password = connectionOptions?.password
 
-    if (url) {
-      debug(`Existing DB client not found; creating one with the provided URL: ${url}`)
+    debug(`Existing DB client not found; creating one with the provided URL`)
+    if (!username && !password) {
+      debug(`No username or password provided; connecting without auth`)
       client = new MongoClient(url)
     } else {
-      debug(
-        `Existing DB client not found; creating one with the following options: host=${host}, port=${port}, username=${username}`,
-      )
-
-      if (!username && !password) {
-        debug(`No username or password provided; connecting without auth`)
-        client = new MongoClient(`mongodb://${host}:${port}`)
-      } else {
-        client = new MongoClient(`mongodb://${host}:${port}`, {
-          auth: {
-            username,
-            password,
-          },
-        })
-      }
+      client = new MongoClient(url, {
+        auth: {
+          username,
+          password,
+        },
+      })
     }
   }
 
