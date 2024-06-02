4dbded378dd72001297b660034eee6bbd20e97d3
Andrew Smith <andrew@andrew.codes>
2024-06-02 15:59:24 +0000

    fix: image creation of playnite-web-app

--- apps/playnite-web/scripts/package.js
+++ apps/playnite-web/scripts/package.js
@@ -1,12 +1,20 @@
-import sh from 'shelljs'
 import { getDockerTags } from 'versioning'
+import fs from 'fs/promises'
+import path from 'path'
+import sh from 'shelljs'
 import pkg from '../package.json' assert { type: 'json' }
 
 async function run() {
   sh.cp('-R', 'build/', '_packaged/')
   sh.cp('-R', 'public/', '_packaged/')
   sh.cp('-R', 'server.mjs/', '_packaged')
-  sh.cp('-R', 'package.json', '_packaged')
+
+  const pkgJson = { ...pkg }
+  delete pkgJson.devDependencies
+  await fs.writeFile(
+    path.join('_packaged', 'package.json'),
+    JSON.stringify(pkgJson, null, 2),
+  )
 
   const { REGISTRY, OWNER, GITHUB_REF } = process.env
 
@@ -18,7 +26,7 @@ async function run() {
 
   for (const tag of tags) {
     sh.exec(
-      `docker build --tag "${REGISTRY}/${OWNER}/${pkg.name}:${tag}" --file Dockerfile .`,
+      `docker build --tag "${REGISTRY}/${OWNER}/${pkg.name}:${tag}" --file Dockerfile ../../`,
     )
   }
 }
