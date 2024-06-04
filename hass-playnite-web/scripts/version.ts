cce4577cd915212882331f4f5426f55bf96a46ec
Andrew Smith <andrew@andrew.codes>
2024-06-04 16:30:22 +0000

    do not version as part of Nx target

--- hass-playnite-web/scripts/version.ts
+++ hass-playnite-web/scripts/version.ts
@@ -1,7 +1,6 @@
 import fs from 'fs'
 import { merge } from 'lodash'
 import path from 'path'
-import sh from 'shelljs'
 import config from '../config.json'
 import pkg from '../package.json'
 
@@ -11,4 +10,3 @@ fs.writeFileSync(
   path.join(__dirname, '..', 'config.json'),
   JSON.stringify(newConfig, null, 2),
 )
-sh.exec(`git add . && git commit --amend --no-edit`)
