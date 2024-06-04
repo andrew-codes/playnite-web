7fb5e04c3c558d02e45421d60e86dc34954e5c71
Andrew Smith <andrew@andrew.codes>
2024-06-04 16:14:10 +0000

    fix: versioning including nx target run

--- release-minor.sh
+++ release-minor.sh
@@ -1,2 +1,3 @@
 yarn lerna version --no-private minor
 yarn nx run-many --target=version --all
+cat package.json | jq '.version' | xargs -I {} git tag "v{}"
--- release-patch.sh
+++ release-patch.sh
@@ -1,2 +1,3 @@
 yarn lerna version --no-private patch
 yarn nx run-many --target=version --all
+cat package.json | jq '.version' | xargs -I {} git tag "v{}"
