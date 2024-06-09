a102810dc2c3a5ea6da45a6cedce27ae8234199b
Andrew Smith <andrew@andrew.codes>
2024-06-09 20:07:55 +0000

    chore(test): test Cypress component tests in main

---
+++ apps/playnite-web/cypress/support/component.ts
@@ -0,0 +1,13 @@
+import 'cypress-plugin-tab'
+import { mount } from 'cypress/react'
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      mount: typeof mount
+    }
+  }
+}
+
+Cypress.Commands.add('mount', mount)
