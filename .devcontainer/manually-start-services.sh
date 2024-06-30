b095f5ac900c8a216e7bb3644006809681bd9710
Andrew Smith <andrew@andrew.codes>
2024-06-30 15:02:39 -0400

    enable local mongodb via docker

--- .devcontainer/manually-start-services.sh
+++ .devcontainer/manually-start-services.sh
@@ -8,9 +8,9 @@ docker container rm playnite-web-db mqtt || true
 mongoDataExists=$(ls .data/mongodb | wc -l)
 mkdir -p /.data/mongodb
 docker run --name playnite-web-db -d \
-  --network host \
-  -e MONGO_INITDB_ROOT_USERNAME=$MONGO_INITDB_ROOT_USERNAME \
-  -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD \
+  -p 27017:27017 \
+  -e MONGO_INITDB_ROOT_USERNAME=$DB_USERNAME \
+  -e MONGO_INITDB_ROOT_PASSWORD=$DB_PASSWORD \
   -v $PWD/.data/mongodb:/data/db \
   -v $PWD/.data/games:/data/backup/games \
   mongo:7.0.3-jammy
