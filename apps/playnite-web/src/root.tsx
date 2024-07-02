0e27b2160dc5967ca8495ad47b3245d261fc9716
Andrew Smith <andrew@andrew.codes>
2024-07-02 10:18:45 -0400

    ensure scrollbar uniformity across browsers
    This addresses visual discrepancies between tests locally on OSX and in CI running on Linux.

--- apps/playnite-web/src/root.tsx
+++ apps/playnite-web/src/root.tsx
@@ -1,5 +1,6 @@
+import { css, Global } from '@emotion/react'
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import { LinksFunction, LoaderFunctionArgs, json } from '@remix-run/node'
+import { json, LinksFunction, LoaderFunctionArgs } from '@remix-run/node'
 import {
   Links,
   Meta,
@@ -145,11 +146,34 @@ const App: FC<{}> = () => {
 
   const outlet = useOutlet()
 
+  const theme = muiTheme(device.type ?? 'unknown')
+
   return (
     <>
       <Head />
-      <ThemeProvider theme={muiTheme(device.type ?? 'unknown')}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
+        <Global
+          styles={css`
+            * {
+              scrollbar-color: ${theme.palette.text.primary}
+                ${theme.palette.background.default};
+            }
+            ::-webkit-scrollbar {
+              background-color: ${theme.palette.background.default};
+            }
+            ::-webkit-scrollbar-thumb {
+              background-color: ${theme.palette.text.primary};
+              border-radius: 10px;
+            }
+            ::-webkit-scrollbar-button {
+              display: none;
+            }
+            ::-webkit-scrollbar-track {
+              background-color: ${theme.palette.background.default};
+            }
+          `}
+        />
         <Layout>
           <AnimatePresence mode="wait" initial={false}>
             <div>{outlet}</div>
