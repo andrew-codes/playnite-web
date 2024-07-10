e4edbc202be84c99c3cf38d30648162b41bb51a1
Andrew Smith <andrew@andrew.codes>
2024-07-10 14:58:17 -0400

    chore: fix CI issues

--- apps/PlayniteWebPlugin/scripts/version.ts
+++ apps/PlayniteWebPlugin/scripts/version.ts
@@ -6,11 +6,8 @@ async function run() {
   process.chdir(path.join(process.cwd(), 'src'))
   const extensionDefinitionContent = await fs.readFile('extension.yaml', 'utf8')
   const extensionDefinition = parse(extensionDefinitionContent)
-  const { VERSION } = process.env
 
-  if (!VERSION) {
-    throw new Error('Missing environment variables')
-  }
+  const { VERSION } = process.env
   extensionDefinition.Version = VERSION ?? extensionDefinition.Version
 
   await fs.writeFile('extension.yaml', stringify(extensionDefinition), 'utf8')
