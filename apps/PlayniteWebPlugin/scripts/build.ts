e9a205dafc3f120b5c6d83ad6e270d9d0c5626b9
Andrew Smith <andrew@andrew.codes>
2024-07-10 14:32:01 -0400

    chore: version extension via semantic versioning mechanism

--- apps/PlayniteWebPlugin/scripts/build.ts
+++ apps/PlayniteWebPlugin/scripts/build.ts
@@ -1,23 +1,8 @@
-import fs from 'fs/promises'
 import path from 'path'
 import sh from 'shelljs'
-import pkg from '../package.json'
 
 async function run() {
   process.chdir(path.join(process.cwd(), 'src'))
-
-  await fs.writeFile('extension.yaml',
-    `
-Id: PlayniteWeb_ec3439e3-51ee-43cb-9a8a-5d82cf45edac
-Name: Playnite Web
-Author: Andrew Smith
-Module: PlayniteWeb.dll
-Type: GenericPlugin
-Icon: Resources\\icon.png
-Version: ${pkg.version}
-`,
-    'utf8',
-  )
 
   sh.exec(`msbuild PlayniteWeb.csproj -property:Configuration=Release`)
 }
