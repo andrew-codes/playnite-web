6f4830a56c6dde4c84a7102d1a734229b8bc8244
Andrew Smith <andrew@andrew.codes>
2024-07-11 12:13:57 -0400

    enable querying graph bi-directional relations

--- apps/playnite-web/src/server/graphql/modules/feature/resolvers/Feature.ts
+++ apps/playnite-web/src/server/graphql/modules/feature/resolvers/Feature.ts
@@ -5,7 +5,7 @@ export const Feature: FeatureResolvers = {
   id: async (_parent, _arg, _ctx) => {
     return create('Feature', _parent.id).toString()
   },
-  gameReleases: async (_parent, _arg, _ctx) => {
-    return _ctx.api.gameRelease.getBy({ featureIds: { $in: _parent.id } })
+  releases: async (_parent, _arg, _ctx) => {
+    return _ctx.api.gameRelease.getBy({ featureIds: _parent.id })
   },
 }
