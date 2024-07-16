517384f7099704121ff9d453bc5274bfc9950be6
Andrew Smith <andrew@andrew.codes>
2024-07-16 09:52:16 -0400

    empty subscription resolver

---
+++ apps/playnite-web/src/server/graphql/modules/gameRelease/resolvers/Subscription/gameRunStateChanged.ts
@@ -0,0 +1,8 @@
+import type { SubscriptionResolvers } from './../../../../types.generated'
+export const gameRunStateChanged: NonNullable<
+  SubscriptionResolvers['gameRunStateChanged']
+> = {
+  subscribe: async (_parent, _arg, _ctx) => {
+    throw new Error('Not implemented')
+  },
+}
