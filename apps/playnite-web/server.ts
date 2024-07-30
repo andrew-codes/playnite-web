b55c250aeab1583185ee31467b59bd81fc17175b
Andrew Smith <andrew@andrew.codes>
2024-07-30 14:59:35 -0400

    include game db updater differently

--- apps/playnite-web/server.ts
+++ apps/playnite-web/server.ts
@@ -2,8 +2,8 @@ import createDebugger from 'debug'
 import dotenv from 'dotenv'
 import { createConnectedMqttClient } from 'mqtt-client'
 import path from 'path'
-import gameDbUpdater from 'playnite-web-game-db-updater'
 import app from './app'
+import gameDbUpdater from './playnite-web-game-db-updater'
 
 dotenv.config({
   path: path.join(process.cwd(), 'local.env'),
@@ -28,7 +28,10 @@ async function run() {
     debug('Starting Playnite Web game-db-updater...')
     gameDbUpdater(
       {
-        assetSaveDirectoryPath: path.join(process.cwd(), 'public/asset-by-id'),
+        assetSaveDirectoryPath: path.join(
+          process.cwd(),
+          'public/assets/asset-by-id',
+        ),
       },
       mqttClient,
     )
