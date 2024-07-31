40c044b30373ffdedaf7683601e3ec9007c3f322
matt-quests-for-tacos <cm.williamsberg@gmail.com>
2024-07-30 20:32:35 -0400

    feat: Added e2e test coverage for playlist home page

--- apps/playnite-web/cypress/e2e/homepage.cy.ts
+++ apps/playnite-web/cypress/e2e/homepage.cy.ts
@@ -4,18 +4,41 @@ describe('Homepage', () => {
     cy.contains('h2', 'Library')
   })
 
-  it(`Displays the "Playing" playlist
+  it(`Shows the On Deck playlist
 - Playlist shows games in a single, horizontally scrolling row.
-- Playing playlist shows games that have the game state: "Playing".`, () => {
+- Each game shows the game's cover image and name.
+- Playing playlist shows games that have the game state: "On Deck".`, () => {
     cy.viewport(1366, 1080)
     cy.visit('/')
-    cy.contains('h4', 'Playing').parents('[data-test="playlist"]')
-    // TODO: Uncomment with implementation of #422
-    // .compareSnapshot({
-    //   name: 'homepage-playing-playlist',
-    //   cypressScreenshotOptions: {
-    //     blackout: ['[data-test="GameFigure"] img'],
-    //   },
-    // })
+    cy.contains('h4', 'On Deck').parents('[data-test="playlist"]')
+    cy.get('[data-test="GameFigure"]').should('have.length', 3)
+    cy.get('[data-test="GameFigure"]').eq(0).should('have.text', "Baldur's Gate 3")
+    cy.get('[data-test="GameFigure"]').eq(1).should('have.text', "Assassin's Creed Mirage")
+    cy.get('[data-test="GameFigure"]').eq(2).should('have.text', "Star Ocean: The Second Story R")
+
+    cy.get('[data-test="playlist"] ul').compareSnapshot({
+      name: 'homepage-playing-playlist',
+      cypressScreenshotOptions: {
+        blackout: ['img'],
+      },
+    })
+
+    cy.get('[data-test="GameFigure"]').eq(0).compareSnapshot({
+      name: 'game-cover-art'
+    })
   })
+
+  it.only(`Displays game details
+    - Opens right navigation drawer when a game is clicked
+    - Game details display the game name and description`, () => {
+        cy.viewport(1366, 1080)
+        cy.visit('/')
+        cy.contains('h4', 'On Deck').parents('[data-test="playlist"]')
+        cy.get('[data-test="GameFigure"] span').eq(0).click({ force: true })
+        cy.get('[data-test="GameDetails"] h4').should('have.text', 'Baldur\'s Gate 3')
+        const description = `An ancient evil has returned to Baldur's Gate, intent on devouring it from the inside out. The fate of Faerun lies in your hands. Alone, you may resist. But together, you can overcome.`
+        cy.get('[data-test="GameDetails"]').should('contain.text', description)
+        cy.get('[name="close-drawer"]').click()
+        cy.get('[data-test="GameDetails"]').should('not.exist')
+      })
 })
