e426eca76937c6614268439cfdba3e92eb9e8347
Andrew Smith <andrew@andrew.codes>
2024-08-08 16:00:23 -0400

    multi-platform builds with sharp npm module

--- apps/playnite-web/scripts/package.ts
+++ apps/playnite-web/scripts/package.ts
@@ -5,32 +5,41 @@ import packageFiles from './utils/packageFiles'
 
 async function run() {
   packageFiles()
-  const { REGISTRY, OWNER, GITHUB_REF, PLATFORM, VERSION, E2E } = process.env
+  const { REGISTRY, OWNER, GITHUB_REF, PLATFORM, VERSION } = process.env
 
   if (!REGISTRY || !OWNER || !GITHUB_REF || !PLATFORM) {
     throw new Error('Missing environment variables')
   }
 
   let tags = await getDockerTags(VERSION ?? null, GITHUB_REF)
+  const platforms = PLATFORM.split(',')
 
-  await Promise.all(
-    tags.map(
-      async (tag) =>
-        new Promise((resolve, reject) => {
-          const command = sh.exec(
-            `docker buildx build --platform ${PLATFORM} --tag "${REGISTRY}/${OWNER}/${pkg.name}:${tag}" --file Dockerfile .`,
-            { async: true },
-          )
-          command.on('exit', (code) => {
-            if (code !== 0) {
-              reject(new Error(`Failed to build image with tag ${tag}`))
-              return
-            }
-            resolve(true)
-          })
-        }),
-    ),
-  )
+  tags.forEach((tag) => {
+    platforms.forEach((platform) => {
+      const cpuArch = platform.includes('amd64')
+        ? 'x64'
+        : platform.includes('arm64')
+          ? 'arm64'
+          : platform.includes('arm')
+            ? 'arm'
+            : 'x64'
+      const baseImage = platform.includes('amd64')
+        ? 'node'
+        : platform.includes('arm64')
+          ? 'arm64v8/node'
+          : platform.includes('arm/v7')
+            ? 'arm32v7/node'
+            : platform.includes('arm/v6')
+              ? 'arm32v6/node'
+              : 'node'
+      console.log(
+        `Building image for ${platform} with cpu_arch ${cpuArch} and docker base image of ${baseImage}`,
+      )
+      sh.exec(
+        `docker buildx build --platform ${platform} --build-arg cpu_arch=${cpuArch} --build-arg base_image=${baseImage} --tag "${REGISTRY}/${OWNER}/${pkg.name}:${tag}" --file Dockerfile .`,
+      )
+    })
+  })
 }
 
 run()
