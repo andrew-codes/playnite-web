99eefa3c047b96f73b8c1450703bfc744d539e6a
Andrew Smith <andrew@andrew.codes>
2024-08-08 16:13:29 -0400

    no longer supporting armv7 and armv6
    The CI freezes when trying to build an arm/v7 or arm/v6 docker image and installing sharp. I can now reproduce this behavior locally.
    
    BREAKING CHANGE: armv6 and armv7 CPUs are no longer supported until further notice.

--- apps/playnite-web/scripts/package.ts
+++ apps/playnite-web/scripts/package.ts
@@ -14,32 +14,37 @@ async function run() {
   let tags = await getDockerTags(VERSION ?? null, GITHUB_REF)
   const platforms = PLATFORM.split(',')
 
-  tags.forEach((tag) => {
-    platforms.forEach((platform) => {
-      const cpuArch = platform.includes('amd64')
-        ? 'x64'
-        : platform.includes('arm64')
-          ? 'arm64'
-          : platform.includes('arm')
-            ? 'arm'
-            : 'x64'
-      const baseImage = platform.includes('amd64')
-        ? 'node'
-        : platform.includes('arm64')
-          ? 'arm64v8/node'
-          : platform.includes('arm/v7')
-            ? 'arm32v7/node'
-            : platform.includes('arm/v6')
-              ? 'arm32v6/node'
-              : 'node'
-      console.log(
-        `Building image for ${platform} with cpu_arch ${cpuArch} and docker base image of ${baseImage}`,
-      )
-      sh.exec(
-        `docker buildx build --platform ${platform} --build-arg cpu_arch=${cpuArch} --build-arg base_image=${baseImage} --tag "${REGISTRY}/${OWNER}/${pkg.name}:${tag}" --file Dockerfile .`,
-      )
-    })
-  })
+  await Promise.all(
+    tags.flatMap((tag) =>
+      platforms.map(
+        async (platform) =>
+          new Promise((resolve) => {
+            const cpuArch = platform.includes('amd64')
+              ? 'x64'
+              : platform.includes('arm64')
+                ? 'arm64'
+                : platform.includes('arm/v7')
+                  ? 'arm32v7'
+                  : platform.includes('arm/v6')
+                    ? 'arm32v6'
+                    : 'x64'
+            const baseImage = platform.includes('amd64')
+              ? 'node'
+              : platform.includes('arm64')
+                ? 'arm64v8/node'
+                : 'node'
+            console.log(
+              `Building image for ${platform} with cpu_arch ${cpuArch} and docker base image of ${baseImage}.`,
+            )
+            const child = sh.exec(
+              `docker buildx build --platform ${platform} --build-arg cpu_arch=${cpuArch} --build-arg base_image=${baseImage} --tag "${REGISTRY}/${OWNER}/${pkg.name}:${tag}" --file Dockerfile .`,
+              { async: true },
+            )
+            child.on('exit', resolve)
+          }),
+      ),
+    ),
+  )
 }
 
 run()
