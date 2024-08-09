efe7b9ec4a3e7772ccb41edd701cd0ea2e903c77
Andrew Smith <andrew@andrew.codes>
2024-08-09 12:51:27 -0400

    do not skip other tests

--- apps/playnite-web/cypress/e2e/homepage.cy.ts
+++ apps/playnite-web/cypress/e2e/homepage.cy.ts
@@ -12,9 +12,15 @@ describe('Homepage', () => {
     cy.visit('/')
     cy.contains('h4', 'On Deck').parents('[data-test="playlist"]')
     cy.get('[data-test="GameFigure"]').should('have.length', 3)
-    cy.get('[data-test="GameFigure"]').eq(0).should('have.text', "Baldur's Gate 3")
-    cy.get('[data-test="GameFigure"]').eq(1).should('have.text', "Assassin's Creed Mirage")
-    cy.get('[data-test="GameFigure"]').eq(2).should('have.text', "Star Ocean: The Second Story R")
+    cy.get('[data-test="GameFigure"]')
+      .eq(0)
+      .should('have.text', "Baldur's Gate 3")
+    cy.get('[data-test="GameFigure"]')
+      .eq(1)
+      .should('have.text', "Assassin's Creed Mirage")
+    cy.get('[data-test="GameFigure"]')
+      .eq(2)
+      .should('have.text', 'Star Ocean: The Second Story R')
 
     cy.get('[data-test="playlist"] ul').compareSnapshot({
       name: 'homepage-playing-playlist',
@@ -24,21 +30,24 @@ describe('Homepage', () => {
     })
 
     cy.get('[data-test="GameFigure"]').eq(0).compareSnapshot({
-      name: 'game-cover-art'
+      name: 'game-cover-art',
     })
   })
 
-  it.only(`Displays game details
+  it(`Displays game details
     - Opens right navigation drawer when a game is clicked
     - Game details display the game name and description`, () => {
-        cy.viewport(1366, 1080)
-        cy.visit('/')
-        cy.contains('h4', 'On Deck').parents('[data-test="playlist"]')
-        cy.get('[data-test="GameFigure"] span').eq(0).click({ force: true })
-        cy.get('[data-test="GameDetails"] h4').should('have.text', 'Baldur\'s Gate 3')
-        const description = `An ancient evil has returned to Baldur's Gate, intent on devouring it from the inside out. The fate of Faerun lies in your hands. Alone, you may resist. But together, you can overcome.`
-        cy.get('[data-test="GameDetails"]').should('contain.text', description)
-        cy.get('[name="close-drawer"]').click()
-        cy.get('[data-test="GameDetails"]').should('not.exist')
-      })
+    cy.viewport(1366, 1080)
+    cy.visit('/')
+    cy.contains('h4', 'On Deck').parents('[data-test="playlist"]')
+    cy.get('[data-test="GameFigure"] span').eq(0).click({ force: true })
+    cy.get('[data-test="GameDetails"] h4').should(
+      'have.text',
+      "Baldur's Gate 3",
+    )
+    const description = `An ancient evil has returned to Baldur's Gate, intent on devouring it from the inside out. The fate of Faerun lies in your hands. Alone, you may resist. But together, you can overcome.`
+    cy.get('[data-test="GameDetails"]').should('contain.text', description)
+    cy.get('[name="close-drawer"]').click()
+    cy.get('[data-test="GameDetails"]').should('not.exist')
+  })
 })
