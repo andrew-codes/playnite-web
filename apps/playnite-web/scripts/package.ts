02bf39c24bb30b29d3c7e26e9402b42d8cce8e3e
Andrew Smith <andrew@andrew.codes>
2024-08-09 12:11:22 -0400

    correctly tag multiple platform docker image

--- apps/playnite-web/scripts/package.ts
+++ apps/playnite-web/scripts/package.ts
@@ -15,34 +15,15 @@ async function run() {
   const platforms = PLATFORM.split(',')
 
   await Promise.all(
-    tags.flatMap((tag) =>
-      platforms.map(
-        async (platform) =>
-          new Promise((resolve) => {
-            const cpuArch = platform.includes('amd64')
-              ? 'x64'
-              : platform.includes('arm64')
-                ? 'arm64'
-                : platform.includes('arm/v7')
-                  ? 'arm32v7'
-                  : platform.includes('arm/v6')
-                    ? 'arm32v6'
-                    : 'x64'
-            const baseImage = platform.includes('amd64')
-              ? 'node'
-              : platform.includes('arm64')
-                ? 'arm64v8/node'
-                : 'node'
-            console.log(
-              `Building image for ${platform} with cpu_arch ${cpuArch} and docker base image of ${baseImage}.`,
-            )
-            const child = sh.exec(
-              `docker buildx build ${process.env.PUBLISH === 'true' ? '--push' : ''} --platform ${platform} --build-arg cpu_arch=${cpuArch} --build-arg base_image=${baseImage} --tag "${REGISTRY}/${OWNER}/${pkg.name}:${tag}" --file Dockerfile .`,
-              { async: true },
-            )
-            child.on('exit', resolve)
-          }),
-      ),
+    tags.flatMap(
+      (tag) =>
+        new Promise((resolve) => {
+          const child = sh.exec(
+            `docker buildx build ${process.env.PUBLISH === 'true' ? '--push' : ''} --platform ${PLATFORM} --tag "${REGISTRY}/${OWNER}/${pkg.name}:${tag}" --file Dockerfile .`,
+            { async: true },
+          )
+          child.on('exit', resolve)
+        }),
     ),
   )
 }
