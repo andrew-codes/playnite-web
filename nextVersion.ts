6a21611bb25b7c2ad80951a85826804146765bdd
Andrew Smith <andrew@andrew.codes>
2024-08-12 16:02:39 -0400

    try js api

---
+++ nextVersion.ts
@@ -0,0 +1,65 @@
+import semanticRelease from 'semantic-release'
+import { WritableStreamBuffer } from 'stream-buffers'
+
+run()
+
+async function run() {
+  const config = {
+    dryRun: true,
+    branches: ['main', 'next'],
+    plugins: [
+      [
+        '@semantic-release/commit-analyzer',
+        {
+          releaseRules: [
+            {
+              breaking: true,
+              release: 'major',
+            },
+            {
+              type: 'feat',
+              release: 'minor',
+            },
+            {
+              type: 'fix',
+              release: 'patch',
+            },
+            {
+              type: 'docs',
+              scope: 'README',
+              release: 'patch',
+            },
+            {
+              type: 'chore',
+              release: 'patch',
+            },
+          ],
+          parserOpts: {
+            noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+          },
+        },
+      ],
+    ],
+  }
+
+  const stdoutBuffer = new WritableStreamBuffer()
+  const stderrBuffer = new WritableStreamBuffer()
+
+  try {
+    const result = await semanticRelease(config, {
+      cwd: process.cwd(),
+      env: { ...process.env },
+      stdout: stdoutBuffer,
+      stderr: stderrBuffer,
+    })
+
+    if (result) {
+      const { nextRelease } = result
+      console.log(nextRelease.version)
+    } else {
+      process.exit(`No result`)
+    }
+  } catch (err) {
+    process.exit(`The automated release failed with ${err}`)
+  }
+}
