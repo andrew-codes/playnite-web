a2555a8f001ac11ee4192c517a3d0eb6ad9143bd
Andrew Smith <andrew@andrew.codes>
2024-08-13 12:01:07 -0400

    fix(ui): 🐛 platform icons show properly in UI
    Platform assets are now copied to the deployed `public/assets`
    directory.
    
    ✅ Closes: #473

--- apps/playnite-web/scripts/utils/packageFiles.ts
+++ apps/playnite-web/scripts/utils/packageFiles.ts
@@ -5,6 +5,8 @@ const run = () => {
   sh.cp('server.production.js', '_packaged/')
   sh.cp('-R', 'build/client', '_packaged/build/')
   sh.rm('-rf', '_packaged/build/client/assets/asset-by-id/*.*')
+  sh.cp('-R', 'public/assets', '_packaged/public/assets')
+  sh.rm('-rf', '_packaged/public/assets/asset-by-id/*.*')
 }
 
 export default run
