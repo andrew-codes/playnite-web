b612cbc0a30cb87756c51a436e6cdf4b71a608df
Andrew Smith <andrew@andrew.codes>
2024-08-13 12:41:14 -0400

    fix(ui): 🐛 fix pathing to platform assets
    ✅ Closes: #473

--- apps/playnite-web/scripts/utils/packageFiles.ts
+++ apps/playnite-web/scripts/utils/packageFiles.ts
@@ -4,8 +4,10 @@ const run = () => {
   sh.mkdir('-p', '_packaged/build')
   sh.cp('server.production.js', '_packaged/')
   sh.cp('-R', 'build/client', '_packaged/build/')
-  sh.rm('-rf', '_packaged/build/client/assets/asset-by-id/*.*')
-  sh.cp('-R', 'public/assets', '_packaged/public/assets')
+  sh.rm('-rf', '_packaged/build/client/assets/asset-by-id')
+
+  sh.mkdir('-p', '_packaged/public/assets')
+  sh.cp('-R', 'public/assets/', '_packaged/public/')
   sh.rm('-rf', '_packaged/public/assets/asset-by-id/*.*')
 }
 
