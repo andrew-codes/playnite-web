9b31e9f5060c1d1bfe47d3a6fd7f7e393370b318
Andrew Smith <andrew@andrew.codes>
2024-08-13 08:56:00 -0400

    Testing game assets are packaged with distributed docker image Fixes #470

--- apps/playnite-web/scripts/utils/packageFiles.ts
+++ apps/playnite-web/scripts/utils/packageFiles.ts
@@ -4,6 +4,7 @@ const run = () => {
   sh.mkdir('-p', '_packaged/build')
   sh.cp('server.production.js', '_packaged/')
   sh.cp('-R', 'build/client', '_packaged/build/')
+  sh.rm('-rf', '_packaged/build/client/assets/asset-by-id/*.*')
 }
 
 export default run
