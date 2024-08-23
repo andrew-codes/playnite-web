a024f084a1d26503dde10e67385f05794e0b9911
Andrew Smith <andrew@andrew.codes>
2024-08-23 13:16:07 -0400

    feat(api): 🎸 enable filtering by release year
    ✅ Closes: #53

--- apps/playnite-web/src/server/graphql/modules/filter/resolvers/Query/filterItems.ts
+++ apps/playnite-web/src/server/graphql/modules/filter/resolvers/Query/filterItems.ts
@@ -1,8 +1,10 @@
-import { uniq } from 'lodash'
+import _ from 'lodash'
 import type {
   FilterItem,
   QueryResolvers,
 } from './../../../../../../../.generated/types.generated'
+
+const { uniq } = _
 
 export const filterItems: NonNullable<QueryResolvers['filterItems']> = async (
   _parent,
@@ -19,7 +21,12 @@ export const filterItems: NonNullable<QueryResolvers['filterItems']> = async (
       releases
         .filter((release) => release.releaseYear)
         .map((release) => release.releaseYear.toString()),
-    ).sort(),
+    )
+      .sort()
+      .map((releaseYear) => ({
+        value: releaseYear,
+        display: releaseYear,
+      })),
     field: 'releaseYear',
   })
 
--- apps/playnite-web/src/server/graphql/modules/game/resolvers/Query/games.ts
+++ apps/playnite-web/src/server/graphql/modules/game/resolvers/Query/games.ts
@@ -1,4 +1,6 @@
+import { WithId } from 'mongodb'
 import type { QueryResolvers } from '../../../../../../../.generated/types.generated'
+import { GameEntity } from '../../../../resolverTypes'
 
 const exactMatch = /(".*")|('.*')/
 
@@ -7,24 +9,79 @@ export const games: NonNullable<QueryResolvers['games']> = async (
   _arg,
   _ctx,
 ) => {
-  return (await _ctx.api.game.getAll())
-    .filter((game) => {
-      if (!_arg.filter) {
-        return true
-      }
+  const matchedGames = (
+    await Promise.all(
+      (await _ctx.api.game.getAll()).reduce(
+        (games, game) => {
+          if (!_arg.filter) {
+            return games.concat(Promise.resolve(game))
+          }
 
-      const filters: Array<boolean> = []
-      if (_arg.filter.name) {
-        if (exactMatch.test(_arg.filter.name)) {
-          filters.push(game.name === _arg.filter.name.slice(1, -1))
-        } else {
-          filters.push(
-            game.name.toLowerCase().includes(_arg.filter.name.toLowerCase()),
+          const filters: Array<boolean> = []
+          if (_arg.filter.name) {
+            if (exactMatch.test(_arg.filter.name)) {
+              filters.push(game.name === _arg.filter.name.slice(1, -1))
+            } else {
+              filters.push(
+                game.name
+                  .toLowerCase()
+                  .includes(_arg.filter.name.toLowerCase()),
+              )
+            }
+          }
+
+          if (_arg.filter.filterItems.length === 0) {
+            if (filters.every((filter) => filter)) {
+              return games.concat(Promise.resolve(game))
+            }
+          }
+
+          return games.concat(
+            Promise.all(
+              _arg.filter.filterItems.map(async (filterItem) => {
+                if (!filterItem?.field) {
+                  return false
+                }
+
+                const releases = await Promise.all(
+                  game.releases.map((release) =>
+                    _ctx.api.gameRelease.getById(release),
+                  ),
+                )
+
+                return releases.some((release) => {
+                  const gameValue = release[filterItem.field]
+                  if (gameValue === undefined) {
+                    return false
+                  }
+
+                  let transformValue = (v) => v
+                  if (typeof gameValue === 'number') {
+                    transformValue = (v) => Number(v)
+                  } else if (typeof gameValue === 'boolean') {
+                    transformValue = (v) => Boolean(v)
+                  } else if (gameValue instanceof Date) {
+                    transformValue = (v) => new Date(v)
+                  }
+
+                  return filterItem?.values.some(
+                    (value) => gameValue === transformValue(value),
+                  )
+                })
+              }),
+            ).then((filterItemResults) => {
+              if (filters.concat(filterItemResults).every((filter) => filter)) {
+                return game
+              }
+
+              return null
+            }),
           )
-        }
-      }
+        },
+        [] as Array<Promise<WithId<GameEntity> | null>>,
+      ),
+    )
+  ).filter((game) => game !== null) as Array<WithId<GameEntity>>
 
-      return filters.every((filter) => filter)
-    })
-    .sort((a, b) => a.name.localeCompare(b.name))
+  return matchedGames.sort((a, b) => a.name.localeCompare(b.name))
 }
