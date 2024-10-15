14ea517765dd6799c7d95a4c29664cd819518118
Andrew Smith <andrew@andrew.codes>
2024-10-15 16:50:00 -0400

    remove poor test
    covered via component tests

--- apps/playnite-web/cypress/e2e/browse/browse.cy.ts
+++ apps/playnite-web/cypress/e2e/browse/browse.cy.ts
@@ -35,28 +35,4 @@ describe('Browse library', () => {
     cy.get('@scrollArea').find('> div').scrollTo('bottom')
     cy.get('@games').contains('figcaption', 'Yakuza: Like a Dragon')
   })
-
-  it('Games have platform icons', () => {
-    cy.visit('/browse')
-    cy.wait('@api')
-    cy.wait('@images')
-
-    cy.get('[data-test="GameFigure"]')
-      .eq(0)
-      .find('[data-test="PlatformListItem"]')
-      .should('have.length', 1)
-    cy.get('[data-test="GameFigure"]')
-      .contains('3DMark')
-      .parents('[data-test="GameFigure"]')
-      .find('[data-test="PlatformListItem"]')
-      .compareSnapshot({
-        name: 'single-platform-icon',
-      })
-
-    cy.get('[data-test="GameFigure"]')
-      .contains('Alan Wake Remastered')
-      .parents('[data-test="GameFigure"]')
-      .find('[data-test="PlatformList"]')
-      .compareSnapshot({ name: 'two-platform-icons' })
-  })
 })
