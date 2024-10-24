b03a1057f859f6255b6a1f4a3199b10b83067931
Andrew Smith <andrew@andrew.codes>
2024-10-24 13:00:38 +0000

    remove volume mount for dev container

--- .devcontainer/manually-start-services.sh
+++ .devcontainer/manually-start-services.sh
@@ -5,19 +5,14 @@ set +o allexport
 docker stop playnite-web-db mqtt || true
 docker container rm playnite-web-db mqtt || true
 
-mongoDataExists=$(ls .data/mongodb | wc -l)
-mkdir -p .data/mongodb
 docker run --name playnite-web-db -d \
   -p 27017:27017 \
   -e MONGO_INITDB_ROOT_USERNAME=$DB_USERNAME \
   -e MONGO_INITDB_ROOT_PASSWORD=$DB_PASSWORD \
-  -v $PWD/.data/mongodb:/data/db \
   -v $PWD/.data/games:/data/backup/games \
   mongo:7.0.3-jammy
 
-if [ $mongoDataExists -eq 0 ]; then
-  docker exec -t playnite-web-db mongorestore --nsInclude games.* /data/backup
-fi
+docker exec -t playnite-web-db mongorestore --nsInclude games.* /data/backup
 
 if [ $(ls apps/playnite-web/public/assets/asset-by-id | wc -l) -eq 0 ]; then
   echo "Copying development environment game assets"
