29992855c671df5331cd5bc8efaf94c2916fc110
Andrew Smith <andrew@andrew.codes>
2024-10-24 14:48:43 +0000

    fix post-create failures

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,23 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-echo -e "{
-\"data-root\": \"/var/lib/docker2\"
-}" >/etc/docker/daemon.json
-/usr/local/share/docker-init.sh
-
 corepack enable
-corepack use yarn@^4.5.1
+corepack use yarn
+yarn set version 4.5.1
 
 echo 'Ensuring Cypress executable is installed'
 { yarn dlx cypress verify; } || { yarn dlx cypress@13.15.0 install; }
-
-echo 'Ensure correct vscode extensions are installed.'
-cat .devcontainer/devcontainer.json |
-  jq -c '.customizations.vscode.extensions[]' |
-  xargs -L 1 code-server --install-extension
-cat .devcontainer/devcontainer.json |
-  jq -c '.customizations.vscode.extensions[]' |
-  xargs -L 1 code --install-extension
 
 git config --global --add safe.directory /workspaces/playnite-web
