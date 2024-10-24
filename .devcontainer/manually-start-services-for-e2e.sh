5358fb2b53571f0fe5bc534826eeba7d3fad5d86
Andrew Smith <andrew@andrew.codes>
2024-10-24 18:56:07 +0000

    tests: fix broken e2e tests in CI

---
+++ .devcontainer/manually-start-services-for-e2e.sh
@@ -0,0 +1,25 @@
+set -o allexport
+. $PWD/apps/playnite-web/local.env
+set +o allexport
+
+docker stop playnite-web-db mqtt || true
+docker container rm playnite-web-db mqtt || true
+
+docker run --name playnite-web-db -d \
+  -p 27017:27017 \
+  -e MONGO_INITDB_ROOT_USERNAME=$DB_USERNAME \
+  -e MONGO_INITDB_ROOT_PASSWORD=$DB_PASSWORD \
+  -v $PWD/.data/games:/data/backup/games \
+  mongo:7.0.3-jammy
+
+docker exec -t playnite-web-db mongorestore --nsInclude games.* /data/backup
+
+rm -rf apps/playnite-web/public/assets-by-id
+cp -r .data/asset-by-id apps/playnite-web/public/assets
+
+docker run --name mqtt -d \
+  -p 1883:1883 \
+  -v $PWD/.data/mqtt/config:/mosquitto/config \
+  eclipse-mosquitto:latest
+
+echo 'Dependent services started.'
