b1d30395d372ef24cc532d3cd7c83b367e9c2eb4
Andrew Smith <andrew@andrew.codes>
2024-11-23 12:14:14 -0500

    fix component tests

--- apps/playnite-web/src/components/Filters/__component_tests__/FilterForm.test.tsx
+++ apps/playnite-web/src/components/Filters/__component_tests__/FilterForm.test.tsx
@@ -1,22 +1,13 @@
-import breakpoints from '../../../../cypress/fixtures/devices.json'
 import { TestWrapper } from '../../../../testUtils/component'
 import FilterForm from '../FilterForm'
 
-Cypress._.each(breakpoints, ([breakpointName, x, y]) => {
-  beforeEach(() => {
-    cy.viewport(x as number, y as number)
-  })
-
-  describe('FilterForm', () => {
-    describe(`Screen size: ${breakpointName}.`, () => {
-      it('Renders', () => {
-        cy.mount(
-          <TestWrapper>
-            <FilterForm />
-          </TestWrapper>,
-        )
-        cy.compareSnapshot(breakpointName as string)
-      })
-    })
+describe('FilterForm', () => {
+  it('Renders', () => {
+    cy.mount(
+      <TestWrapper>
+        <FilterForm />
+      </TestWrapper>,
+    )
+    cy.compareSnapshot('FilterForm-renders')
   })
 })
