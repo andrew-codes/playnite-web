0f78fef4b0aece68772e013294f39d631ef562e8
Andrew Smith <andrew@andrew.codes>
2024-11-26 12:59:36 -0500

    fixes

--- apps/playnite-web/scripts/component-for-update.ts
+++ apps/playnite-web/scripts/component-for-update.ts
@@ -1,7 +1,14 @@
 import sh from 'shelljs'
 
 async function run() {
-  sh.exec(`yarn nx run playnite-web-app:test/components:ci/local`)
+  const [, , specFilter] = process.argv
+  sh.exec(
+    `yarn cypress run --component --browser electron ${specFilter && `--spec src/**/__component_tests__/${specFilter}`}`,
+  )
+
+  sh.exec(
+    'yarn cypress-image-diff-html-report start --reportJsonDir visual-regression-tests/component-report --autoOpen',
+  )
 }
 
 run()
