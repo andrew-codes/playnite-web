aac5e90b00dcacd11a9008fd63ecd4967b6b4d30
Andrew Smith <andrew@andrew.codes>
2024-12-09 08:45:41 -0500

    better debug logs for library sync

--- apps/playnite-web/src/server/mqttUpdater/index.ts
+++ apps/playnite-web/src/server/mqttUpdater/index.ts
@@ -6,6 +6,8 @@ import { IDeleteQuery, IQuery, IUpdateQuery } from '../data/types.api.js'
 import { PubSubChannels } from '../graphql/subscriptionPublisher.js'
 import handlers from './handlers/index.js'
 
+const debug = createDebugger('playnite-web/game-db-updater/index')
+
 type HandlerOptions = {
   assetSaveDirectoryPath: string
   pubsub: PubSub<PubSubChannels>
@@ -16,21 +18,36 @@ type HandlerOptions = {
 }
 
 const batchTopic = /^playnite\/[^/]+\/batch$/
+const librarySync = /^playnite\/[^/]+\/library\/sync\/(.*)$/
 
 const mqttUpdater = async (options: HandlerOptions): Promise<void> => {
-  const debug = createDebugger('playnite-web/game-db-updater/index')
   debug('Starting game-db-updater')
 
   options.mqtt.subscribe('playnite/#')
 
   await fs.mkdir(options.assetSaveDirectoryPath, { recursive: true })
 
+  let syncMessageCount = 0
   options.mqtt.on('message', async (topic, payload) => {
+    const librarySyncMessageMatches = librarySync.exec(topic)
+    if (librarySyncMessageMatches?.[1] === 'start') {
+      syncMessageCount = 0
+      return
+    }
+    if (librarySyncMessageMatches?.[1] === 'completed') {
+      debug(
+        `Library sync completed. Processed a total of ${syncMessageCount} database entities. This number should match the number of all documents, across all collections, in your games database.`,
+      )
+      return
+    }
+
     let messages: Array<{ topic: string; payload: Buffer }> = []
     if (batchTopic.test(topic)) {
       messages = JSON.parse(payload.toString()).messages
-      console.log('Batched messages received', messages.length)
+      syncMessageCount += messages.length
+      debug('Batched messages received', messages.length)
     } else {
+      syncMessageCount += 1
       messages.push({ topic, payload })
     }
 
