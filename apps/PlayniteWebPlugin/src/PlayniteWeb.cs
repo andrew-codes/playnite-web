c4a431769f580cce9ca4f443782151572986d388
Andrew Smith <andrew@andrew.codes>
2025-02-01 21:36:20 -0500

    chore: enable updating other Playnite entity types

--- apps/PlayniteWebPlugin/src/PlayniteWeb.cs
+++ apps/PlayniteWebPlugin/src/PlayniteWeb.cs
@@ -12,6 +12,7 @@ using PlayniteWeb.Services.Publishers;
 using PlayniteWeb.Services.Publishers.Mqtt;
 using PlayniteWeb.Services.Subscribers;
 using PlayniteWeb.Services.Subscribers.Mqtt;
+using PlayniteWeb.Services.Updaters;
 using PlayniteWeb.TopicManager;
 using PlayniteWeb.UI;
 using System;
@@ -58,6 +59,7 @@ namespace PlayniteWeb
     private readonly Regex pcExpression = new Regex("Windows.*");
     private readonly IEnumerable<Platform> pcPlatforms;
     private readonly string _version;
+    private readonly EntityUpdater entityUpdater;
     private bool IsReconnectInProgress = false;
     private bool IsShutDownInProgress = false;
     private readonly GameSource emulatorSource = new GameSource("Emulator");
@@ -75,6 +77,8 @@ namespace PlayniteWeb
       var extensionInfoYaml = System.IO.File.ReadAllText(Path.Combine(api.Paths.ExtensionsDataPath, "..", "Extensions", $"PlayniteWeb_{this.Id}", "extension.yaml"));
       var extension = Serialization.FromYaml<Dictionary<string, string>>(extensionInfoYaml);
       _version = extension["Version"];
+
+      entityUpdater = new EntityUpdater();
 
       mqtt = new MqttFactory().CreateMqttClient();
       settings = new PlayniteWebSettingsViewModel(this);
@@ -379,7 +383,7 @@ namespace PlayniteWeb
       }
       catch (Exception ex)
       {
-        logger.Debug($"No process found for Game {e.Name} with ID {e.ProcessId}. Shutting down game by matching its process name instead.");
+        logger.Error(ex, $"Error occurred in Subscriber_OnStopRelease for Game ID {e.Id}.");
         var process = Process.GetProcessesByName(e.Name).FirstOrDefault();
         if (process == null)
         {
@@ -426,74 +430,49 @@ namespace PlayniteWeb
     {
       try
       {
-        var collection = typeof(IGameDatabase).GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty).First(info => info.Name == e.EntityTypeName).GetValue(PlayniteApi.Database);
         var id = Guid.Parse(e.EntityId);
-        var entity = ((IItemCollection<Playnite.SDK.Models.Game>)collection).Get(Guid.Parse(e.EntityId));
 
-        var fields = e.Fields;
-        foreach (var field in fields)
-        {
-          try
-          {
-            if (field.Key == "Id" || field.Key == "Name")
-            {
-              logger.Warn($"Field {field.Key} is not updatable.");
-              continue;
-            }
-
-            var property = entity.GetType().GetProperty(field.Key);
-            if (field.Value == null)
-            {
-              logger.Debug($"Property {field.Key} not found on entity {e.EntityTypeName} with ID {e.EntityId}.");
-              continue;
-            }
-            if (property.PropertyType == typeof(Guid) || property.PropertyType == typeof(Nullable<Guid>))
-            {
-              property.SetValue(entity, Guid.Parse(field.Value.ToString()));
-            }
-            else if (property.PropertyType == typeof(string))
-            {
-              property.SetValue(entity, field.Value.ToString());
-            }
-            else if (property.PropertyType == typeof(int) || property.PropertyType == typeof(Nullable<int>))
-            {
-              property.SetValue(entity, int.Parse(field.Value.ToString()));
-            }
-            else if (property.PropertyType == typeof(bool) || property.PropertyType == typeof(Nullable<bool>))
-            {
-              property.SetValue(entity, bool.Parse(field.Value.ToString()));
-            }
-            else if (property.PropertyType == typeof(DateTime) || property.PropertyType == typeof(Nullable<DateTime>))
-            {
-              property.SetValue(entity, DateTime.Parse(field.Value.ToString()));
-            }
-            else if (property.PropertyType == typeof(List<Guid>))
-            {
-              property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).Select(Guid.Parse).ToList());
-            }
-            else if (property.PropertyType == typeof(List<string>))
-            {
-              property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).ToList());
-            }
-            else if (property.PropertyType == typeof(List<int>))
-            {
-              property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).Select(int.Parse).ToList());
-            }
-            else if (property.PropertyType == typeof(List<bool>))
-            {
-              property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).Select(bool.Parse).ToList());
-            }
-            else if (property.PropertyType == typeof(List<DateTime>))
-            {
-              property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).Select(DateTime.Parse).ToList());
-            }
-          }
-          catch (Exception ex)
-          {
-            logger.Error(ex, $"Error occurred in Subscriber_OnUpdateEntity for Entity ID {e.EntityId}. Could not process property {field.Key}");
-          }
+        if (e.EntityTypeName == "Game") {
+          var collection = PlayniteApi.Database.Games;
+          var entity = collection.Get(Guid.Parse(e.EntityId));
+          entity = entityUpdater.Update<Playnite.SDK.Models.Game>(entity, e.Fields);
+          collection.Update(entity);
         }
-        ((IItemCollection<Playnite.SDK.Models.Game>)collection).Update(entity);
+        else if (e.EntityTypeName == "Platform")
+        {
+          var collection = PlayniteApi.Database.Platforms;
+          var entity = collection.Get(Guid.Parse(e.EntityId));
+          entity = entityUpdater.Update<Platform>(entity, e.Fields);
+          collection.Update(entity);
+        }
+        else if (e.EntityTypeName == "Genre")
+        {
+          var collection = PlayniteApi.Database.Genres;
+          var entity = collection.Get(Guid.Parse(e.EntityId));
+          entity = entityUpdater.Update<Genre>(entity, e.Fields);
+          collection.Update(entity);
+        }
+        else if (e.EntityTypeName == "Tag")
+        {
+          var collection = PlayniteApi.Database.Tags;
+          var entity = collection.Get(Guid.Parse(e.EntityId));
+          entity = entityUpdater.Update<Tag>(entity, e.Fields);
+          collection.Update(entity);
+        }
+        else if (e.EntityTypeName == "GameFeature")
+        {
+          var collection = PlayniteApi.Database.Features;
+          var entity = collection.Get(Guid.Parse(e.EntityId));
+          entity = entityUpdater.Update<GameFeature>(entity, e.Fields);
+          collection.Update(entity);
+        }
+        else if (e.EntityTypeName == "Series")
+        {
+          var collection = PlayniteApi.Database.Series;
+          var entity = collection.Get(Guid.Parse(e.EntityId));
+          entity = entityUpdater.Update<Series>(entity, e.Fields);
+          collection.Update(entity);
+        }
       }
       catch (Exception ex)
       {
---
+++ apps/PlayniteWebPlugin/src/Services/Updaters/EntityUpdater.cs
@@ -0,0 +1,93 @@
+using Playnite.SDK;
+using Playnite.SDK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlayniteWeb.Services.Updaters
+{
+  internal class EntityUpdater
+  {
+    private readonly ILogger logger;
+
+    public EntityUpdater()
+    {
+      logger = LogManager.GetLogger();
+    }
+
+    public TEntityType Update<TEntityType>(TEntityType entity, IDictionary<string, object> values)
+    {
+      foreach (var field in values)
+      {
+        try
+        {
+          if (field.Key == "Id" || field.Key == "Name")
+          {
+            logger.Warn($"Field {field.Key} is not updatable.");
+            continue;
+          }
+
+          var property = entity.GetType().GetProperty(field.Key);
+          if (property == null)
+          {
+            logger.Debug($"Property {field.Key} not found on entity {typeof(TEntityType).Name} with ID {((IIdentifiable)entity).Id}.");
+            continue;
+          }
+
+          if (field.Value == null)
+          {
+            logger.Debug($"Property {field.Key} not found on entity {typeof(TEntityType).Name} with ID {((IIdentifiable)entity).Id}.");
+            continue;
+          }
+
+          if (property.PropertyType == typeof(Guid) || property.PropertyType == typeof(Nullable<Guid>))
+          {
+            property.SetValue(entity, Guid.Parse(field.Value.ToString()));
+          }
+          else if (property.PropertyType == typeof(string))
+          {
+            property.SetValue(entity, field.Value.ToString());
+          }
+          else if (property.PropertyType == typeof(int) || property.PropertyType == typeof(Nullable<int>))
+          {
+            property.SetValue(entity, int.Parse(field.Value.ToString()));
+          }
+          else if (property.PropertyType == typeof(bool) || property.PropertyType == typeof(Nullable<bool>))
+          {
+            property.SetValue(entity, bool.Parse(field.Value.ToString()));
+          }
+          else if (property.PropertyType == typeof(DateTime) || property.PropertyType == typeof(Nullable<DateTime>))
+          {
+            property.SetValue(entity, DateTime.Parse(field.Value.ToString()));
+          }
+          else if (property.PropertyType == typeof(List<Guid>))
+          {
+            property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).Select(Guid.Parse).ToList());
+          }
+          else if (property.PropertyType == typeof(List<string>))
+          {
+            property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).ToList());
+          }
+          else if (property.PropertyType == typeof(List<int>))
+          {
+            property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).Select(int.Parse).ToList());
+          }
+          else if (property.PropertyType == typeof(List<bool>))
+          {
+            property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).Select(bool.Parse).ToList());
+          }
+          else if (property.PropertyType == typeof(List<DateTime>))
+          {
+            property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).Select(DateTime.Parse).ToList());
+          }
+        }
+        catch (Exception ex)
+        {
+          logger.Error(ex, $"Error occurred in Subscriber_OnUpdateEntity for Entity ID {((IIdentifiable)entity).Id}. Could not process property {field.Key}");
+        }
+      }
+
+      return entity;
+    }
+  }
+}
