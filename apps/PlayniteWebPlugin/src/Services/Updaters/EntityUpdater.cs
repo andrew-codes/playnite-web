1486b8338af0519d763c7433be0073024786f0ab
Andrew Smith <andrew@andrew.codes>
2025-02-02 14:19:37 -0500

    playnite game features can be updated from Playnite Web

--- apps/PlayniteWebPlugin/src/Services/Updaters/EntityUpdater.cs
+++ apps/PlayniteWebPlugin/src/Services/Updaters/EntityUpdater.cs
@@ -1,18 +1,22 @@
 using Playnite.SDK;
 using Playnite.SDK.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 
 namespace PlayniteWeb.Services.Updaters
 {
   internal class EntityUpdater
   {
     private readonly ILogger logger;
+    private readonly IPlayniteAPI api;
 
-    public EntityUpdater()
+    public EntityUpdater(IPlayniteAPI api)
     {
       logger = LogManager.GetLogger();
+      this.api = api;
     }
 
     public TEntityType Update<TEntityType>(TEntityType entity, IDictionary<string, object> values) where TEntityType : DatabaseObject, IIdentifiable
@@ -43,45 +47,79 @@ namespace PlayniteWeb.Services.Updaters
           if (property.PropertyType == typeof(Guid) || property.PropertyType == typeof(Nullable<Guid>))
           {
             property.SetValue(entity, Guid.Parse(field.Value.ToString()));
+            entity.OnPropertyChanged(field.Key);
           }
           else if (property.PropertyType == typeof(string))
           {
             property.SetValue(entity, field.Value.ToString());
+            entity.OnPropertyChanged(field.Key);
           }
           else if (property.PropertyType == typeof(int) || property.PropertyType == typeof(Nullable<int>))
           {
             property.SetValue(entity, int.Parse(field.Value.ToString()));
+            entity.OnPropertyChanged(field.Key);
           }
           else if (property.PropertyType == typeof(bool) || property.PropertyType == typeof(Nullable<bool>))
           {
             property.SetValue(entity, bool.Parse(field.Value.ToString()));
+            entity.OnPropertyChanged(field.Key);
           }
           else if (property.PropertyType == typeof(DateTime) || property.PropertyType == typeof(Nullable<DateTime>))
           {
             property.SetValue(entity, DateTime.Parse(field.Value.ToString()));
+            entity.OnPropertyChanged(field.Key);
           }
-          else if (property.PropertyType == typeof(List<Guid>))
+          else
           {
-            property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).Select(Guid.Parse).ToList());
-          }
-          else if (property.PropertyType == typeof(List<string>))
-          {
-            property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).ToList());
-          }
-          else if (property.PropertyType == typeof(List<int>))
-          {
-            property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).Select(int.Parse).ToList());
-          }
-          else if (property.PropertyType == typeof(List<bool>))
-          {
-            property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).Select(bool.Parse).ToList());
-          }
-          else if (property.PropertyType == typeof(List<DateTime>))
-          {
-            property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).Select(DateTime.Parse).ToList());
+            var updateValues = ((JsonElement)field.Value).Deserialize<IDictionary<string, IEnumerable<string>>>();
+            var addedValues = updateValues["added"].Distinct();
+            var removedValues = updateValues["removed"].Distinct();
+
+
+            if (property.PropertyType == typeof(List<Guid>))
+            {
+              var addedIds = addedValues.Select(v => Guid.Parse(v)).ToList();
+              var removedIds = removedValues.Select(v => Guid.Parse(v)).ToList();
+
+              if (entity.GetType() == typeof(Game))
+              {
+                var game = entity as Game;
+                if (field.Key == "FeatureIds")
+                {
+                  foreach (var guid in addedIds)
+                  {
+                    game.FeatureIds.Add(guid);
+                    game.Features.Add(api.Database.Features.FirstOrDefault(f => f.Id.Equals(guid)));
+                  }
+                  foreach (var guid in removedIds)
+                  {
+                    game.FeatureIds.Remove(guid);
+                    game.Features.Remove(api.Database.Features.FirstOrDefault(f => f.Id.Equals(guid)));
+                  }
+
+                  entity.OnPropertyChanged(field.Key);
+                  entity.OnPropertyChanged("Features");
+                }
+              }
+            }
+            else if (property.PropertyType == typeof(List<string>))
+            {
+              property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).ToList());
+            }
+            else if (property.PropertyType == typeof(List<int>))
+            {
+              property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).Select(int.Parse).ToList());
+            }
+            else if (property.PropertyType == typeof(List<bool>))
+            {
+              property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).Select(bool.Parse).ToList());
+            }
+            else if (property.PropertyType == typeof(List<DateTime>))
+            {
+              property.SetValue(entity, ((IEnumerable<object>)field.Value).Select(v => v.ToString()).Select(DateTime.Parse).ToList());
+            }
           }
 
-          entity.OnPropertyChanged(field.Key);
         }
         catch (Exception ex)
         {
