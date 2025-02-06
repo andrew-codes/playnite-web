5f43a69e632af2e60622db38a283cec27fcbc710
Andrew Smith <andrew@andrew.codes>
2025-02-05 21:29:05 -0500

    e2e test verifications

--- apps/playnite-web/cypress/e2e/browse/update-completion-status.cy.ts
+++ apps/playnite-web/cypress/e2e/browse/update-completion-status.cy.ts
@@ -2,11 +2,11 @@ describe('Browse', () => {
   describe('Update completion status.', () => {
     beforeEach(() => {
       cy.intercept('POST', '/api').as('api')
-      cy.visit('/browse')
-      cy.wait('@api')
     })
 
     it(`Requires being signed in.`, () => {
+      cy.visit('/browse')
+      cy.wait('@api')
       cy.get('[data-test="GameFigure"]')
         .eq(0)
         .as('gameFigure')
@@ -15,6 +15,23 @@ describe('Browse', () => {
       cy.get('@gameFigure').find('li:contains("Paused")').click({ force: true })
       cy.get('[data-test="GameFigure"]').compareSnapshot(
         'update-completion-status-requires-being-signed-in',
+      )
+    })
+
+    it(`Updates completion status.`, () => {
+      cy.signIn()
+      cy.visit('/browse')
+      cy.wait('@api')
+      cy.get('[data-test="GameFigure"]')
+        .eq(0)
+        .as('gameFigure')
+        .find('[data-test="GameFigureChipList"] button')
+        .click()
+      cy.get('@gameFigure').parent().find('li:contains("Beaten")').click()
+      cy.wait('@api')
+      cy.get('@gameFigure').contains(
+        '[data-test="GameFigureChipList"]',
+        'Beaten',
       )
     })
   })
