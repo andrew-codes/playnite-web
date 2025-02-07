5898915518c3fd795022e6b5a4f309b3b80fcb4b
Andrew Smith <andrew@andrew.codes>
2025-02-06 19:36:55 -0500

    passing tests

--- apps/playnite-web/cypress/e2e/browse/update-completion-status.cy.ts
+++ apps/playnite-web/cypress/e2e/browse/update-completion-status.cy.ts
@@ -5,32 +5,46 @@ describe('Browse', () => {
       cy.intercept('POST', '/api').as('api')
     })
 
+    beforeEach(() => {
+      cy.task('updateDatabase', {
+        collection: 'release',
+        filter: {
+          name: '3DMark',
+        },
+        update: {
+          $set: { completionStatusId: '9c5f2de1-6be5-4751-8dbe-a64ff2bafaa7' },
+        },
+      })
+    })
+
     it(`Requires being signed in.`, () => {
       cy.visit('/browse')
       cy.wait('@api')
       cy.get('[data-test="GameFigure"]')
-        .eq(0)
-        .as('gameFigure')
         .find('[data-test="GameFigureChipList"] button')
         .should('not.exist')
-      cy.get('@gameFigure').find('li:contains("Paused")').click({ force: true })
-      cy.get('[data-test="GameFigure"]').compareSnapshot(
-        'update-completion-status-requires-being-signed-in',
-      )
+      cy.get('[data-test="GameFigure"]')
+        .contains('3DMark')
+        .parents('[data-test="GameFigure"]')
+        .contains('[data-test="GameFigureChipList"]', 'Completed')
+        .eq(0)
+        .click({ force: true })
     })
 
     it(`Update completion status.`, () => {
       cy.signIn()
+
       cy.visit('/browse')
       cy.wait('@api')
       cy.get('[data-test="GameFigure"]')
-        .eq(0)
+        .contains('3DMark')
+        .parents('[data-test="GameFigure"]')
         .as('gameFigure')
-        .find('[data-test="GameFigureChipList"] button')
+      cy.get('@gameFigure')
+        .contains('[data-test="GameFigureChipList"] button', 'Completed')
         .click()
-      cy.get('@gameFigure').parent().find('li:contains("Beaten")').eq(0).click()
-      cy.wait(2000)
-      // cy.wait('@api')
+      cy.get('@gameFigure').contains('li', 'Beaten').eq(0).click()
+      cy.wait('@api')
       cy.get('@gameFigure').contains(
         '[data-test="GameFigureChipList"]',
         'Beaten',
