2038bae12b444b64f7a6a659afe9110ad5bca74e
Andrew Smith <andrew@andrew.codes>
2025-02-07 13:30:02 -0500

    fix: completion status selection is not broken when browsing

--- apps/playnite-web/cypress/e2e/browse/update-completion-status.cy.ts
+++ apps/playnite-web/cypress/e2e/browse/update-completion-status.cy.ts
@@ -15,6 +15,15 @@ describe('Browse', () => {
           $set: { completionStatusId: '9c5f2de1-6be5-4751-8dbe-a64ff2bafaa7' },
         },
       })
+      cy.task('updateDatabase', {
+        collection: 'release',
+        filter: {
+          name: 'killer7',
+        },
+        update: {
+          $set: { completionStatusId: '9c5f2de1-6be5-4751-8dbe-a64ff2bafaa7' },
+        },
+      })
     })
 
     it(`Requires being signed in.`, () => {
@@ -38,6 +47,32 @@ describe('Browse', () => {
       cy.wait('@api')
       cy.get('[data-test="GameFigure"]')
         .contains('3DMark')
+        .parents('[data-test="GameFigure"]')
+        .as('gameFigure')
+      cy.get('@gameFigure')
+        .contains('[data-test="GameFigureChipList"] button', 'Completed')
+        .click()
+      cy.get('@gameFigure').contains('li', 'Beaten').eq(0).click()
+      cy.wait('@api')
+      cy.get('@gameFigure').contains(
+        '[data-test="GameFigureChipList"]',
+        'Beaten',
+      )
+    })
+
+    it.only(`Scrolling, update completion status.`, () => {
+      cy.signIn()
+
+      cy.visit('/browse')
+      cy.wait('@api')
+      cy.get('[data-test="GameFigure"]').as('games')
+      cy.get('@games')
+        .parents('.MuiBox-root')
+        .eq(0)
+        .find('> div')
+        .scrollTo('bottom')
+      cy.get('[data-test="GameFigure"]')
+        .contains('killer7')
         .parents('[data-test="GameFigure"]')
         .as('gameFigure')
       cy.get('@gameFigure')
