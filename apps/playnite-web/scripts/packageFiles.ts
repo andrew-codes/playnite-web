c29bd46e4fba0ad99f4e6a776c57cbb2cc25ef6e
Andrew Smith <andrew@andrew.codes>
2025-08-10 15:32:36 -0400

    clean up and apply learnings to dev server target

--- apps/playnite-web/scripts/packageFiles.ts
+++ apps/playnite-web/scripts/packageFiles.ts
@@ -1,4 +1,49 @@
-import packageFiles from './utils/packageFiles'
+import fs from 'fs'
+import { globSync } from 'glob'
+import sh from 'shelljs'
 
-packageFiles()
-console.log('Packaged files completed successfully')
+const run = () => {
+  if (!fs.existsSync('build') || !fs.existsSync('_build-output')) {
+    console.error('Build files not found. Please build the project first.')
+    process.exit(1)
+  }
+
+  console.log('Copying built files to _packaged')
+  sh.mkdir('-p', '_packaged/src/server')
+  sh.exec('cp -R _build-output/ _packaged')
+  sh.exec('cp -R build/client/ _packaged/src/client')
+
+  console.log('Modifying imports of generated files')
+  globSync('_packaged/.generated/*.js').forEach((file: string) => {
+    let contents: string = fs.readFileSync(file, 'utf8')
+
+    const writeContents = contents
+      .split('\n')
+      .map((line) => {
+        const matched = /import\s+(.*)\s+from\s+['"](\.\.?\/)(.+)['"];/gm.exec(
+          line,
+        )
+        return matched
+          ? `import ${matched[1]} from '${matched[2]}${matched[3]}.js';`
+          : line
+      })
+      .join('\n')
+    fs.writeFileSync(file, writeContents, 'utf8')
+  })
+
+  console.log('Copying and modifying package.json')
+  const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+  pkg.name = `packaged-${pkg.name}`
+  pkg.devDependencies = {}
+  fs.writeFileSync(
+    '_packaged/package.json',
+    JSON.stringify(pkg, null, 2),
+    'utf8',
+  )
+
+  sh.mkdir('-p', '_packaged/src/public/assets')
+  console.log('Copying assets')
+  sh.exec('cp -r src/public/assets/* _packaged/src/public/assets')
+}
+
+run()
---
+++ apps/playnite-web/scripts/startDevServer.ts
@@ -0,0 +1,103 @@
+import nodemon from 'nodemon'
+import path from 'path'
+import sh from 'shelljs'
+
+async function setup() {
+  return Promise.all([
+    new Promise((resolve, reject) => {
+      sh.exec(
+        `yarn pnpify prisma generate --schema=src/server/data/providers/postgres/schema.prisma`,
+        { async: true },
+        (code, stdout, stderr) => {
+          if (code !== 0) {
+            reject(new Error(`Failed to generate prisma client: ${stderr}`))
+          } else {
+            resolve(stdout)
+          }
+        },
+      )
+    }),
+    new Promise((resolve, reject) => {
+      sh.exec(
+        `yarn graphql-codegen --config codegen.ts`,
+        { async: true },
+        (code, stdout, stderr) => {
+          if (code !== 0) {
+            reject(new Error(`Failed to run graphql codegen: ${stderr}`))
+          } else {
+            resolve(stdout)
+          }
+        },
+      )
+    }),
+    new Promise((resolve, reject) => {
+      sh.exec(
+        `yarn nx db/push playnite-web-app`,
+        { async: true },
+        (code, stdout, stderr) => {
+          if (code !== 0) {
+            reject(new Error(`Failed to push database: ${stderr}`))
+          } else {
+            resolve(stdout)
+          }
+        },
+      )
+    }),
+    new Promise((resolve, reject) => {
+      sh.exec(
+        `kill -9 $(lsof -t -i:24678`,
+        { async: true },
+        (code, stdout, stderr) => {
+          if (code !== 0) {
+            reject(new Error(`Failed to push database: ${stderr}`))
+          } else {
+            resolve(stdout)
+          }
+        },
+      )
+    }),
+  ])
+}
+async function run() {
+  await setup()
+
+  nodemon({
+    script: path.join(__dirname, '..', '..', 'src', 'server', 'server.ts'),
+    ext: 'ts tsx js jsx json graphql env',
+    execMap: {
+      js: 'yarn tsx',
+      ts: 'yarn tsx',
+    },
+    watch: [
+      path.join(__dirname, '..', '..', 'src', 'server', '*.*'),
+      path.join(__dirname, '..', '..', 'src', 'server', '**', '*.*'),
+      path.join(__dirname, '..', '..', '*.env'),
+      path.join(__dirname, '..', '..', 'codegen.ts'),
+    ],
+    ignore: [
+      path.join(__dirname, '..', '..', 'src', '**', '*.webp'),
+      path.join(__dirname, '..', '..', 'src', '**', '*.png'),
+      path.join(__dirname, '..', '..', 'src', '**', '*.jpg'),
+      path.join(__dirname, '..', '..', 'src', '**', '*.jpeg'),
+      path.join(__dirname, '..', '..', 'src', '**', '*.gif'),
+    ],
+    env: {
+      NODE_ENV: 'development',
+      LOG_LEVEL: process.env.LOG_LEVEL ?? '',
+    },
+  })
+
+  nodemon
+    .on('start', function () {
+      console.log('App is building...')
+    })
+    .on('quit', function () {
+      console.log('App has quit')
+      process.exit()
+    })
+    .on('restart', function (files) {
+      setup()
+    })
+}
+
+run()
