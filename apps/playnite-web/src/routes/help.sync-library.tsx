65d4c736e93a42dba6abc620ae8fe216b4f04749
Andrew Smith <andrew@andrew.codes>
2025-08-15 21:00:52 -0400

    more visual updates

--- apps/playnite-web/src/routes/help.sync-library.tsx
+++ apps/playnite-web/src/routes/help.sync-library.tsx
@@ -1,15 +1,14 @@
+import { Typography } from '@mui/material'
 import { FC } from 'react'
-import Layout from '../components/Layout'
-import MainNavigation from '../components/Navigation/MainNavigation'
 
 const HelpSyncLibrary: FC<{}> = () => {
   return (
-    <Layout navs={[MainNavigation]}>
-      <div className="help-sync-library">
-        <h2>Sync Library</h2>
-        <p>Here you can manage your library synchronization settings.</p>
-      </div>
-    </Layout>
+    <div className="help-sync-library">
+      <Typography variant="h2">Sync Library</Typography>
+      <Typography variant="body1">
+        Here you can manage your library synchronization settings.
+      </Typography>
+    </div>
   )
 }
 
--- apps/playnite-web/src/routes/help.tsx
+++ apps/playnite-web/src/routes/help.tsx
@@ -1,18 +1,24 @@
+import { Typography } from '@mui/material'
 import { Outlet } from '@remix-run/react'
 import { FC } from 'react'
-import Drawer from '../components/Navigation/Drawer'
-import OuterContainer from '../components/OuterContainer'
+import Header from '../components/Header'
+import Layout from '../components/Layout'
+import MainNavigation from '../components/Navigation/MainNavigation'
 
 const Help: FC<{}> = () => {
   return (
-    <Drawer>
-      <OuterContainer>
-        <div className="help-page">
-          <h1>Get Help</h1>
-          <Outlet />
-        </div>
-      </OuterContainer>
-    </Drawer>
+    <Layout
+      navs={[MainNavigation]}
+      title={
+        <Header>
+          <Typography variant="h1">Help</Typography>
+        </Header>
+      }
+    >
+      <div className="help-page">
+        <Outlet />
+      </div>
+    </Layout>
   )
 }
 
