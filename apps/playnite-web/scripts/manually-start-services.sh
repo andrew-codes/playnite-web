11e6f705d5957a14d7b37a00d6498bdcc9f7bc11
Andrew Smith <andrew@andrew.codes>
2025-08-24 10:13:30 -0400

    start db free of data always

--- apps/playnite-web/scripts/manually-start-services.sh
+++ apps/playnite-web/scripts/manually-start-services.sh
@@ -2,16 +2,6 @@ echo "Starting dependent services..."
 
 REPO_ROOT=$PWD/../..
 
-docker container rm db --force || true
-
-docker run --name db -d \
-  -p 5432:5432 \
-  -e POSTGRES_USER=local \
-  -e POSTGRES_PASSWORD=dev \
-  -v $REPO_ROOT/.data/postgres:/var/lib/postgresql/data \
-  -v $REPO_ROOT/.data/games:/data/backup/games \
-  postgres:latest
-
 echo 'Dependent services started.'
 echo "Starting dependent services..."
 
@@ -23,8 +13,6 @@ docker run --name db -d \
   -p 5432:5432 \
   -e POSTGRES_USER=local \
   -e POSTGRES_PASSWORD=dev \
-  -v $REPO_ROOT/.data/postgres:/var/lib/postgresql/data \
-  -v $REPO_ROOT/.data/games:/data/backup/games \
   postgres:latest
 
 echo "Waiting for PostgreSQL to be ready..."
