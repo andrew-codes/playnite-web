fe7f7f8ac535895cdf73d0df04b8141c0940b02e
Andrew Smith <andrew@andrew.codes>
2025-08-25 14:52:52 -0400

    fix deletion order for entities on library sync

--- apps/playnite-web/src/server/graphql/modules/library/resolvers/Mutation/syncLibrary.ts
+++ apps/playnite-web/src/server/graphql/modules/library/resolvers/Mutation/syncLibrary.ts
@@ -74,21 +74,6 @@ export const syncLibrary: NonNullable<
   })
   logger.debug(`Removed ${removedFeatures.count} features.`)
 
-  //  Sources
-  logger.debug(
-    `Removing sources from library ${libraryId}`,
-    _arg.libraryData.remove.sources,
-  )
-  const removedSources = await _ctx.db.source.deleteMany({
-    where: {
-      libraryId,
-      playniteId: {
-        in: _arg.libraryData.remove.sources,
-      },
-    },
-  })
-  logger.debug(`Removed ${removedSources.count} sources.`)
-
   // Platforms
   logger.debug(
     `Removing platforms from library ${libraryId}`,
@@ -103,6 +88,21 @@ export const syncLibrary: NonNullable<
     },
   })
   logger.debug(`Removed ${deletedPlatforms.count} platforms.`)
+
+  //  Sources
+  logger.debug(
+    `Removing sources from library ${libraryId}`,
+    _arg.libraryData.remove.sources,
+  )
+  const removedSources = await _ctx.db.source.deleteMany({
+    where: {
+      libraryId,
+      playniteId: {
+        in: _arg.libraryData.remove.sources,
+      },
+    },
+  })
+  logger.debug(`Removed ${removedSources.count} sources.`)
 
   // Tags
   logger.debug(
