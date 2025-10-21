66c8e892a6533f18000e9e36c2f68435d009df87
Andrew Smith <andrew@andrew.codes>
2025-10-21 16:10:02 -0400

    fix: restore dynamic payload support in graph serializer

--- apps/PlayniteWebPlugin/src/Services/GraphSerializer.cs
+++ apps/PlayniteWebPlugin/src/Services/GraphSerializer.cs
@@ -2,9 +2,12 @@ using GraphQL;
 using GraphQL.Client.Abstractions.Websocket;
 using Playnite.SDK;
 using System;
+using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,13 +15,17 @@ namespace PlayniteWeb.Services
 {
   internal class GraphSerializer : IGraphQLWebsocketJsonSerializer
   {
-    private JsonSerializerOptions _options;
+    private readonly JsonSerializerOptions _options;
 
     public GraphSerializer() {
-            _options = new JsonSerializerOptions(JsonSerializerDefaults.Web)
-            {
-              Converters = { new TypeConverter() }
-            };
+      _options = new JsonSerializerOptions(JsonSerializerDefaults.Web)
+      {
+        Converters =
+        {
+          new TypeConverter(),
+          new DynamicObjectConverter()
+        }
+      };
     }
 
     public Task<GraphQLResponse<TResponse>> DeserializeFromUtf8StreamAsync<TResponse>(Stream stream, CancellationToken cancellationToken)
@@ -82,6 +89,86 @@ namespace PlayniteWeb.Services
         var json = sr.ReadToEnd();
 
         return Task.FromResult(JsonSerializer.Deserialize<T>(Encoding.UTF8.GetBytes(json), _options));
+      }
+    }
+
+    private class DynamicObjectConverter : JsonConverter<object>
+    {
+      public override bool CanConvert(Type typeToConvert)
+      {
+        return typeToConvert == typeof(object);
+      }
+
+      public override object? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+      {
+        using var document = JsonDocument.ParseValue(ref reader);
+        return ToDynamic(document.RootElement);
+      }
+
+      public override void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options)
+      {
+        JsonSerializer.Serialize(writer, value, value.GetType(), options);
+      }
+
+      private static object? ToDynamic(JsonElement element)
+      {
+        switch (element.ValueKind)
+        {
+          case JsonValueKind.Object:
+            var expando = new ExpandoObject() as IDictionary<string, object?>;
+            foreach (var property in element.EnumerateObject())
+            {
+              expando[property.Name] = ToDynamic(property.Value);
+            }
+
+            return expando;
+
+          case JsonValueKind.Array:
+            var list = new List<object?>();
+            foreach (var item in element.EnumerateArray())
+            {
+              list.Add(ToDynamic(item));
+            }
+
+            return list;
+
+          case JsonValueKind.String:
+            if (element.TryGetDateTime(out var dateTime))
+            {
+              return dateTime;
+            }
+
+            if (element.TryGetGuid(out var guid))
+            {
+              return guid;
+            }
+
+            return element.GetString();
+
+          case JsonValueKind.Number:
+            if (element.TryGetInt64(out var longValue))
+            {
+              if (longValue >= int.MinValue && longValue <= int.MaxValue)
+              {
+                return (int)longValue;
+              }
+
+              return longValue;
+            }
+
+            return element.GetDouble();
+
+          case JsonValueKind.True:
+          case JsonValueKind.False:
+            return element.GetBoolean();
+
+          case JsonValueKind.Null:
+          case JsonValueKind.Undefined:
+            return null;
+
+          default:
+            return null;
+        }
       }
     }
   }
