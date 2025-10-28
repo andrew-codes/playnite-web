4feee0ccb4e4449f4ce5e6262c4d0fe710e6cce1
Andrew Smith <andrew@andrew.codes>
2025-10-28 09:09:04 -0400

    cleanup and claude md updates

--- apps/PlayniteWebMqttPlugin/src/Services/Publishers/GameStatePayload.cs
+++
@@ -1,9 +0,0 @@
-namespace PlayniteWebMqtt.Services.Publishers
-{
-  internal class GameStatePayload
-  {
-    public int? ProcessId { get; set; }
-    public string GameId {get; set; }
-    public string State { get; set; }
-  }
-}
