9b5f8adb9d3dacd3dacdc5d0cdaacd66bf5ebc26
Andrew Smith <andrew@andrew.codes>
2025-12-20 16:34:34 -0500

    fix prisma migrations

--- libs/db-client/src/prisma.config.ts
+++ libs/db-client/src/prisma.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, env } from 'prisma/config'
 export default defineConfig({
   schema: path.join(__dirname, 'schema.prisma'),
   migrations: {
-    path: path.join(__dirname, 'prisma/migrations'),
+    path: path.join(__dirname, 'migrations'),
   },
   datasource: {
     url: env('DATABASE_URL'),
