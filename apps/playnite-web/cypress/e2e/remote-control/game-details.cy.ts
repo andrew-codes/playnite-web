8a391623c86c4f4cea353ff9c6c96e775ca7675e
Andrew Smith <andrew@andrew.codes>
2025-12-23 20:33:22 -0500

    fix test

--- apps/playnite-web/cypress/e2e/remote-control/game-details.cy.ts
+++ apps/playnite-web/cypress/e2e/remote-control/game-details.cy.ts
@@ -25,216 +25,230 @@ describe(`Game details remote control.
       cy.signIn('test', 'test')
     })
 
-    cy.get('[data-test="Actions"]', { timeout: 10000 })
-      .children()
-      .should('have.length', 0)
-  })
-
-  describe('With Webhook setting.', () => {
-    beforeEach(() => {
-      cy.task('setUserSettings', {
-        username: 'test',
-        settings: {
-          webhook: 'http://localhost:3000/echo',
-        },
-      })
-    })
-
-    beforeEach(() => {
+    it(`Non-owned library.
+            - No action controls.`, () => {
       cy.fixture('librarySync.json')
         .then((libraryData) => {
-          return cy.syncLibrary('test', 'test', libraryData)
+          cy.syncLibrary('test', 'test', libraryData)
+          cy.syncLibrary('jane', 'jane', libraryData)
         })
         .then((library) => {
           cy.signIn('test', 'test')
-          cy.visit(`/u/test/${library.body.data.syncLibrary.id}`)
-          cy.waitForImages(40)
+          cy.visit(`/u/jane/${library.body.data.syncLibrary.id}`)
         })
+      cy.get('[data-test="GameFigure"] button img').eq(0).click({ force: true })
+
+      cy.get('[data-test="Actions"]', { timeout: 10000 })
+        .children()
+        .should('have.length', 0)
     })
 
-    it(`Play button.
+    describe('With Webhook setting.', () => {
+      beforeEach(() => {
+        cy.task('setUserSettings', {
+          username: 'test',
+          settings: {
+            webhook: 'http://localhost:3000/echo',
+          },
+        })
+      })
+
+      beforeEach(() => {
+        cy.fixture('librarySync.json')
+          .then((libraryData) => {
+            return cy.syncLibrary('test', 'test', libraryData)
+          })
+          .then((library) => {
+            cy.signIn('test', 'test')
+            cy.visit(`/u/test/${library.body.data.syncLibrary.id}`)
+            cy.waitForImages(40)
+          })
+      })
+
+      it(`Play button.
           - Is visible.
           - Buttons to play on each source of the game.
           - Graph mutation posts to webhook.
           - Playing triggers restart and stop buttons to appear.`, () => {
-      cy.contains("Assassin's Creed Odyssey")
-        .parents('[data-test=GameFigure]')
-        .find('button img')
-        .eq(0)
-        .click({ force: true })
+        cy.contains("Assassin's Creed Odyssey")
+          .parents('[data-test=GameFigure]')
+          .find('button img')
+          .eq(0)
+          .click({ force: true })
 
-      cy.get('[data-test="Actions"] button', { timeout: 10000 })
-        .eq(0)
-        .contains('PC (Windows) via Steam')
-      cy.get('[data-test="Actions"] button').last().click()
+        cy.get('[data-test="Actions"] button', { timeout: 10000 })
+          .eq(0)
+          .contains('PC (Windows) via Steam')
+        cy.get('[data-test="Actions"] button').last().click()
 
-      cy.get('[data-test="Actions"] li')
-        .eq(1)
-        .contains('PC (Windows) via Steam')
-      cy.get('[data-test="Actions"] li')
-        .eq(2)
-        .contains('Sony PlayStation 5 via PlayStation')
+        cy.get('[data-test="Actions"] li')
+          .eq(1)
+          .contains('PC (Windows) via Steam')
+        cy.get('[data-test="Actions"] li')
+          .eq(2)
+          .contains('Sony PlayStation 5 via PlayStation')
 
-      cy.intercept('POST', '/api', (req) => {
-        if (req.body.operationName !== 'startRelease') {
-          return
-        }
-        cy.get('[data-test="Actions"] button').then(($btn) => {
-          expect(req.body.query).to.include('startRelease')
-          expect(req.body.variables.id).to.equal($btn.data('release-id'))
-        })
-      })
-      cy.get('[data-test="Actions"] li')
-        .eq(2)
-        .contains('Sony PlayStation 5 via PlayStation')
-        .click()
-
-      cy.get('[data-test="Actions"] button', { timeout: 10000 }).eq(0).click()
-      cy.wait('@api')
-
-      // Wait a moment for the webhook to be posted.
-      cy.wait(5000)
-
-      cy.request({
-        method: 'GET',
-        url: '/echo',
-      }).then((response) => {
-        cy.get('[data-test="Actions"] button').then(($btn) => {
-          const event = response.body
-          expect(event.type).to.equal('StartReleaseRequested')
-          expect(event.payload).to.nested.include({
-            id: $btn.data('release-id'),
-            title: "Assassin's Creed Odyssey",
-            'library.name': 'Game Room',
-            'platform.name': 'Sony PlayStation 5',
-            'source.name': 'PlayStation',
+        cy.intercept('POST', '/api', (req) => {
+          if (req.body.operationName !== 'startRelease') {
+            return
+          }
+          cy.get('[data-test="Actions"] button').then(($btn) => {
+            expect(req.body.query).to.include('startRelease')
+            expect(req.body.variables.id).to.equal($btn.data('release-id'))
           })
-          expect(event.payload.coverUrl).to.match(/^\/cover-art\/.*\.webp$/)
-          expect(event.payload.library.id).to.match(/Library:\d+/)
-          expect(event.payload.platform.id).to.match(/Platform:\d+/)
-          expect(event.payload.source.id).to.match(/Source:\d+/)
         })
+        cy.get('[data-test="Actions"] li')
+          .eq(2)
+          .contains('Sony PlayStation 5 via PlayStation')
+          .click()
+
+        cy.get('[data-test="Actions"] button', { timeout: 10000 }).eq(0).click()
+        cy.wait('@api')
+
+        // Wait a moment for the webhook to be posted.
+        cy.wait(5000)
+
+        cy.request({
+          method: 'GET',
+          url: '/echo',
+        }).then((response) => {
+          cy.get('[data-test="Actions"] button').then(($btn) => {
+            const event = response.body
+            expect(event.type).to.equal('StartReleaseRequested')
+            expect(event.payload).to.nested.include({
+              id: $btn.data('release-id'),
+              title: "Assassin's Creed Odyssey",
+              'library.name': 'Game Room',
+              'platform.name': 'Sony PlayStation 5',
+              'source.name': 'PlayStation',
+            })
+            expect(event.payload.coverUrl).to.match(/^\/cover-art\/.*\.webp$/)
+            expect(event.payload.library.id).to.match(/Library:\d+/)
+            expect(event.payload.platform.id).to.match(/Platform:\d+/)
+            expect(event.payload.source.id).to.match(/Source:\d+/)
+          })
+        })
+
+        cy.get('[data-test="Actions"] li')
+          .eq(1)
+          .contains('button', 'Restart game')
+        cy.get('[data-test="Actions"] li').eq(2).contains('button', 'Stop game')
       })
 
-      cy.get('[data-test="Actions"] li')
-        .eq(1)
-        .contains('button', 'Restart game')
-      cy.get('[data-test="Actions"] li').eq(2).contains('button', 'Stop game')
-    })
-
-    it(`Stop button.
+      it(`Stop button.
           - Is visible for a starting or started game.
           - Graph mutation posts to webhook.
           - Stop and restart buttons disappear.`, () => {
-      cy.contains("Assassin's Creed Odyssey")
-        .parents('[data-test=GameFigure]')
-        .find('button img')
-        .click({ force: true })
+        cy.contains("Assassin's Creed Odyssey")
+          .parents('[data-test=GameFigure]')
+          .find('button img')
+          .click({ force: true })
 
-      cy.get('[data-test="Actions"] button', { timeout: 10000 })
-        .eq(0)
-        .contains('PC (Windows) via Steam')
-      cy.get('[data-test="Actions"] button').last().click()
+        cy.get('[data-test="Actions"] button', { timeout: 10000 })
+          .eq(0)
+          .contains('PC (Windows) via Steam')
+        cy.get('[data-test="Actions"] button').last().click()
 
-      cy.get('[data-test="Actions"] li')
-        .eq(1)
-        .contains('PC (Windows) via Steam')
-      cy.get('[data-test="Actions"] li')
-        .eq(2)
-        .contains('Sony PlayStation 5 via PlayStation')
+        cy.get('[data-test="Actions"] li')
+          .eq(1)
+          .contains('PC (Windows) via Steam')
+        cy.get('[data-test="Actions"] li')
+          .eq(2)
+          .contains('Sony PlayStation 5 via PlayStation')
 
-      cy.get('[data-test="Actions"] li')
-        .eq(2)
-        .contains('Sony PlayStation 5 via PlayStation')
-        .click()
+        cy.get('[data-test="Actions"] li')
+          .eq(2)
+          .contains('Sony PlayStation 5 via PlayStation')
+          .click()
 
-      cy.get('[data-test="Actions"] button', { timeout: 10000 }).eq(0).click()
-      cy.get('[data-test="Actions"] li')
-        .eq(2)
-        .contains('button', 'Stop game')
-        .click()
-      cy.get('[data-test="Actions"] li').should('have.length', 1)
-      cy.wait('@api')
+        cy.get('[data-test="Actions"] button', { timeout: 10000 }).eq(0).click()
+        cy.get('[data-test="Actions"] li')
+          .eq(2)
+          .contains('button', 'Stop game')
+          .click()
+        cy.get('[data-test="Actions"] li').should('have.length', 1)
+        cy.wait('@api')
 
-      // Wait a moment for the webhook to be posted.
-      cy.wait(5000)
+        // Wait a moment for the webhook to be posted.
+        cy.wait(5000)
 
-      cy.request({
-        method: 'GET',
-        url: '/echo',
-      }).then((response) => {
-        cy.get('[data-test="Actions"] button').then(($btn) => {
-          const event = response.body
-          expect(event.type).to.equal('StopReleaseRequested')
-          expect(event.payload).to.nested.include({
-            id: $btn.data('release-id'),
-            title: "Assassin's Creed Odyssey",
-            'library.name': 'Game Room',
-            'platform.name': 'Sony PlayStation 5',
-            'source.name': 'PlayStation',
+        cy.request({
+          method: 'GET',
+          url: '/echo',
+        }).then((response) => {
+          cy.get('[data-test="Actions"] button').then(($btn) => {
+            const event = response.body
+            expect(event.type).to.equal('StopReleaseRequested')
+            expect(event.payload).to.nested.include({
+              id: $btn.data('release-id'),
+              title: "Assassin's Creed Odyssey",
+              'library.name': 'Game Room',
+              'platform.name': 'Sony PlayStation 5',
+              'source.name': 'PlayStation',
+            })
+            expect(event.payload.coverUrl).to.match(/^\/cover-art\/.*\.webp$/)
+            expect(event.payload.library.id).to.match(/Library:\d+/)
+            expect(event.payload.platform.id).to.match(/Platform:\d+/)
+            expect(event.payload.source.id).to.match(/Source:\d+/)
           })
-          expect(event.payload.coverUrl).to.match(/^\/cover-art\/.*\.webp$/)
-          expect(event.payload.library.id).to.match(/Library:\d+/)
-          expect(event.payload.platform.id).to.match(/Platform:\d+/)
-          expect(event.payload.source.id).to.match(/Source:\d+/)
         })
       })
-    })
 
-    it(`Restart button.
+      it(`Restart button.
           - Is visible for a starting or started game.
           - Graph mutation posts to webhook.
           - All controls remain visible.`, () => {
-      cy.contains("Assassin's Creed Odyssey")
-        .parents('[data-test=GameFigure]')
-        .find('button img')
-        .click({ force: true })
+        cy.contains("Assassin's Creed Odyssey")
+          .parents('[data-test=GameFigure]')
+          .find('button img')
+          .click({ force: true })
 
-      cy.get('[data-test="Actions"] button', { timeout: 10000 })
-        .eq(0)
-        .contains('PC (Windows) via Steam')
-      cy.get('[data-test="Actions"] button').last().click()
+        cy.get('[data-test="Actions"] button', { timeout: 10000 })
+          .eq(0)
+          .contains('PC (Windows) via Steam')
+        cy.get('[data-test="Actions"] button').last().click()
 
-      cy.get('[data-test="Actions"] li')
-        .eq(1)
-        .contains('PC (Windows) via Steam')
-      cy.get('[data-test="Actions"] li')
-        .eq(2)
-        .contains('Sony PlayStation 5 via PlayStation')
+        cy.get('[data-test="Actions"] li')
+          .eq(1)
+          .contains('PC (Windows) via Steam')
+        cy.get('[data-test="Actions"] li')
+          .eq(2)
+          .contains('Sony PlayStation 5 via PlayStation')
 
-      cy.get('[data-test="Actions"] li')
-        .eq(2)
-        .contains('Sony PlayStation 5 via PlayStation')
-        .click()
+        cy.get('[data-test="Actions"] li')
+          .eq(2)
+          .contains('Sony PlayStation 5 via PlayStation')
+          .click()
 
-      cy.get('[data-test="Actions"] button', { timeout: 10000 }).eq(0).click()
-      cy.get('[data-test="Actions"] li')
-        .eq(1)
-        .contains('button', 'Restart game')
-        .click()
-      cy.wait('@api')
+        cy.get('[data-test="Actions"] button', { timeout: 10000 }).eq(0).click()
+        cy.get('[data-test="Actions"] li')
+          .eq(1)
+          .contains('button', 'Restart game')
+          .click()
+        cy.wait('@api')
 
-      // Wait a moment for the webhook to be posted.
-      cy.wait(5000)
+        // Wait a moment for the webhook to be posted.
+        cy.wait(5000)
 
-      cy.request({
-        method: 'GET',
-        url: '/echo',
-      }).then((response) => {
-        cy.get('[data-test="Actions"] button').then(($btn) => {
-          const event = response.body
-          expect(event.type).to.equal('RestartReleaseRequested')
-          expect(event.payload).to.nested.include({
-            id: $btn.data('release-id'),
-            title: "Assassin's Creed Odyssey",
-            'library.name': 'Game Room',
-            'platform.name': 'Sony PlayStation 5',
-            'source.name': 'PlayStation',
+        cy.request({
+          method: 'GET',
+          url: '/echo',
+        }).then((response) => {
+          cy.get('[data-test="Actions"] button').then(($btn) => {
+            const event = response.body
+            expect(event.type).to.equal('RestartReleaseRequested')
+            expect(event.payload).to.nested.include({
+              id: $btn.data('release-id'),
+              title: "Assassin's Creed Odyssey",
+              'library.name': 'Game Room',
+              'platform.name': 'Sony PlayStation 5',
+              'source.name': 'PlayStation',
+            })
+            expect(event.payload.coverUrl).to.match(/^\/cover-art\/.*\.webp$/)
+            expect(event.payload.library.id).to.match(/Library:\d+/)
+            expect(event.payload.platform.id).to.match(/Platform:\d+/)
+            expect(event.payload.source.id).to.match(/Source:\d+/)
           })
-          expect(event.payload.coverUrl).to.match(/^\/cover-art\/.*\.webp$/)
-          expect(event.payload.library.id).to.match(/Library:\d+/)
-          expect(event.payload.platform.id).to.match(/Platform:\d+/)
-          expect(event.payload.source.id).to.match(/Source:\d+/)
         })
       })
     })
