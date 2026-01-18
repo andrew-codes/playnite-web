663ef518b328a0ec603dc07608ec92bc8b523334
Andrew Smith <andrew@andrew.codes>
2026-01-18 11:07:40 -0500

    feat: updating a game's run state via graph API invokes webhook
    Closes #948

--- apps/playnite-web/src/server/graphql/modules/release/resolvers/Mutation/updateRelease.ts
+++ apps/playnite-web/src/server/graphql/modules/release/resolvers/Mutation/updateRelease.ts
@@ -2,12 +2,15 @@ import { GraphQLError } from 'graphql'
 import { Source } from '../../../../../data/providers/postgres/client'
 import logger from '../../../../../logger'
 import {
+  create,
+  createNull,
   domains,
   hasIdentity,
   Identity,
   IIdentify,
   tryParseOid,
 } from '../../../../../oid'
+import { defaultSettings as defaultUserSettings } from '../../../../../userSettings'
 import type { MutationResolvers } from './../../../../../../../.generated/types.generated'
 
 export const updateRelease: NonNullable<
@@ -82,6 +85,15 @@ export const updateRelease: NonNullable<
     const release = await _ctx.db.release.update({
       where: {
         id: releaseId.id,
+      },
+      include: {
+        Library: true,
+        Game: true,
+        Source: {
+          include: {
+            Platform: true,
+          },
+        },
       },
       data: {
         description: _arg.release.description,
@@ -161,6 +173,63 @@ export const updateRelease: NonNullable<
           }),
         playniteId: release.playniteId,
       })
+    }
+
+    if (
+      _arg.release.runState === 'running' ||
+      _arg.release.runState === 'stopped'
+    ) {
+      const webhookSetting = await _ctx.db.userSetting.findUnique({
+        where: {
+          userId_name: {
+            name: defaultUserSettings.webhook.id,
+            userId: user.id.id,
+          },
+        },
+        select: {
+          value: true,
+        },
+      })
+
+      if (webhookSetting?.value) {
+        await fetch(JSON.parse(webhookSetting.value as string), {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            type:
+              _arg.release.runState === 'running'
+                ? 'StartReleaseRequested'
+                : 'StopReleaseRequested',
+            payload: {
+              id: create('Release', release.id),
+              title: release.title,
+              coverUrl: release.Game.coverArt
+                ? `/cover-art/${release.Game.coverArt}`
+                : null,
+              playniteId: release.playniteId,
+              library: {
+                id: create('Library', release.Library.id),
+                name: release.Library.name,
+                playniteId: release.Library.playniteId,
+              },
+              platform: {
+                id: release.Source?.Platform?.id
+                  ? create('Platform', release.Source.Platform.id)
+                  : createNull('Platform'),
+                name: release.Source?.Platform?.name ?? '',
+                playniteId: release.Source?.Platform?.playniteId,
+              },
+              source: {
+                id: create('Source', release.Source.id),
+                name: release.Source.name,
+                playniteId: release.Source.playniteId,
+              },
+            },
+          }),
+        })
+      }
     }
 
     return release
